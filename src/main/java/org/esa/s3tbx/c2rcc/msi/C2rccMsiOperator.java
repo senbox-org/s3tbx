65d8f05b027b9542000e7d801a0cfb60b0e8668c
Marco Peters <marco.peters@brockmann-consult.de>
2016-05-29 18:30:24 +0200

    Issue #10: Value range for parameters is too broad - MSI

--- src/main/java/org/esa/s3tbx/c2rcc/msi/C2rccMsiOperator.java
+++ src/main/java/org/esa/s3tbx/c2rcc/msi/C2rccMsiOperator.java
@@ -215,16 +215,16 @@ public class C2rccMsiOperator extends PixelOperator implements C2rccConfigurable
             converter = BooleanExpressionConverter.class)
     private String validPixelExpression;
 
-    @Parameter(defaultValue = "35.0", unit = "PSU", interval = "(0, 100)")
+    @Parameter(defaultValue = "35.0", unit = "PSU", interval = "(0.1, 43)")
     private double salinity;
 
-    @Parameter(defaultValue = "15.0", unit = "C", interval = "(-50, 50)")
+    @Parameter(defaultValue = "15.0", unit = "C", interval = "(0.1, 36)")
     private double temperature;
 
     @Parameter(defaultValue = "330", unit = "DU", interval = "(0, 1000)")
     private double ozone;
 
-    @Parameter(defaultValue = "1000", unit = "hPa", interval = "(0, 2000)", label = "Air Pressure")
+    @Parameter(defaultValue = "1000", unit = "hPa", interval = "(800, 1040)", label = "Air Pressure")
     private double press;
 
     @Parameter(defaultValue = "1000", unit = "m", interval = "(0, 8500)", label = "Elevation")
