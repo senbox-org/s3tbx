87c3464e3acb190cca0eb26a6778102a6bdac217
Marco Peters <marco.peters@brockmann-consult.de>
2016-05-29 18:40:40 +0200

    Issue #10: Value range for parameters is too broad - MERIS

--- src/main/java/org/esa/s3tbx/c2rcc/meris/C2rccMerisOperator.java
+++ src/main/java/org/esa/s3tbx/c2rcc/meris/C2rccMerisOperator.java
@@ -240,16 +240,16 @@ public class C2rccMerisOperator extends PixelOperator implements C2rccConfigurab
             converter = BooleanExpressionConverter.class)
     private String validPixelExpression;
 
-    @Parameter(defaultValue = "35.0", unit = "PSU", interval = "(0, 100)")
+    @Parameter(defaultValue = "35.0", unit = "PSU", interval = "(0.1, 43)")
     private double salinity;
 
-    @Parameter(defaultValue = "15.0", unit = "C", interval = "(-50, 50)")
+    @Parameter(defaultValue = "15.0", unit = "C", interval = "(0.1, 36)")
     private double temperature;
 
     @Parameter(defaultValue = "330", unit = "DU", interval = "(0, 1000)")
     private double ozone;
 
-    @Parameter(defaultValue = "1000", unit = "hPa", interval = "(0, 2000)", label = "Air Pressure")
+    @Parameter(defaultValue = "1000", unit = "hPa", interval = "(800, 1040)", label = "Air Pressure")
     private double press;
 
     @Parameter(defaultValue = "1.72", description = "Conversion factor bpart. (TSM = bpart * TSMfakBpart + bwit * TSMfakBwit)", label = "TSM factor bpart")
