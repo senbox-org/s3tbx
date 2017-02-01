7211ad4ddbf196cb1e5dc098d0ee7baa7b88bb42
Sean Bailey <sean.w.bailey@nasa.gov>
2017-02-01 14:16:39 +0100

    added a new VIIRS L1B reader to provide geolocation for VIIRS add support for triple_window SST flags
    Signed-off-by: Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>

---
+++ seadas-reader/src/main/java/gov/nasa/gsfc/seadas/dataio/L1BViirsProductReaderPlugin.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.nasa.gsfc.seadas.dataio;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import org.esa.snap.dataio.netcdf.util.NetcdfFileOpener;
+import org.esa.snap.core.dataio.DecodeQualification;
+import org.esa.snap.core.dataio.ProductReader;
+import org.esa.snap.core.dataio.ProductReaderPlugIn;
+import org.esa.snap.core.util.io.SnapFileFilter;
+import ucar.nc2.Attribute;
+import ucar.nc2.NetcdfFile;
+
+/**
+ *
+ * @author sean
+ */
+public class L1BViirsProductReaderPlugin implements ProductReaderPlugIn {
+        // Set to "true" to output debugging information.
+    // Don't forget to setback to "false" in production code!
+    //
+    private static final boolean DEBUG = false;
+
+    private static final String DEFAULT_FILE_EXTENSION_L1B_MOD = ".L1B-M_SNPP.nc";
+    private static final String DEFAULT_FILE_EXTENSION_L1B_IMG = ".L1B-I_SNPP.nc";
+    private static final String DEFAULT_FILE_EXTENSION_L1B_DNB = ".L1B-D_SNPP.nc";
+
+    public static final String READER_DESCRIPTION = "VIIRS L1B Products (NASA)";
+    public static final String FORMAT_NAME = "VIIRS-L1B";
+
+//    private static final String[] supportedProductTypes = {
+//            "MODIS_SWATH_Type_L1B",
+//    };
+//    private static final Set<String> supportedProductTypeSet = new HashSet<String>(Arrays.asList(supportedProductTypes));
+
+    /**
+     * Checks whether the given object is an acceptable input for this product reader and if so, the method checks if it
+     * is capable of decoding the input's content.
+     */
+    @Override
+    public DecodeQualification getDecodeQualification(Object input) {
+        final File file = SeadasProductReader.getInputFile(input);
+        if (file == null) {
+            return DecodeQualification.UNABLE;
+        }
+        if (!file.exists()) {
+            if (DEBUG) {
+                System.out.println("# File not found: " + file);
+            }
+            return DecodeQualification.UNABLE;
+        }
+        if (!file.isFile()) {
+            if (DEBUG) {
+                System.out.println("# Not a file: " + file);
+            }
+            return DecodeQualification.UNABLE;
+        }
+        NetcdfFile ncfile = null;
+        try {
+            ncfile = NetcdfFileOpener.open(file.getPath());
+            if (ncfile != null) {
+               Attribute instrumentName = ncfile.findGlobalAttribute("instrument");
+               Attribute processingLevel = ncfile.findGlobalAttribute("processing_level");
+
+                if (instrumentName != null) {
+                    if (processingLevel != null) {
+                        if (instrumentName.getStringValue().equals("VIIRS") && processingLevel.getStringValue().equals("L1B")) {
+                            if (DEBUG) {
+                                System.out.println(file);
+                            }
+                            ncfile.close();
+                            return DecodeQualification.INTENDED;
+                        } 
+                    } else {
+                            if (DEBUG) {
+                                System.out.println("# Missing processing_level attribute: " + file);
+                            }
+                        }
+                } else {
+                    if (DEBUG) {
+                        System.out.println("# Missing instrument attribute': " + file);
+                    }
+                }
+            } else {
+                if (DEBUG) {
+                    System.out.println("# Can't open as NetCDF: " + file);
+                }
+            }
+        } catch (Exception ignore) {
+            if (DEBUG) {
+                System.out.println("# I/O exception caught: " + file);
+            }
+        } finally {
+            if (ncfile != null) {
+                try {
+                    ncfile.close();
+                } catch (IOException ignore) {
+                }
+            }
+        }
+        return DecodeQualification.UNABLE;
+    }
+
+    /**
+     * Returns an array containing the classes that represent valid input types for this reader.
+     * <p/>
+     * <p> Intances of the classes returned in this array are valid objects for the <code>setInput</code> method of the
+     * <code>ProductReader</code> interface (the method will not throw an <code>InvalidArgumentException</code> in this
+     * case).
+     *
+     * @return an array containing valid input types, never <code>null</code>
+     */
+    @Override
+    public Class[] getInputTypes() {
+        return new Class[]{String.class, File.class};
+    }
+
+    /**
+     * Creates an instance of the actual product reader class. This method should never return <code>null</code>.
+     *
+     * @return a new reader instance, never <code>null</code>
+     */
+    @Override
+    public ProductReader createReaderInstance() {
+        return new SeadasProductReader(this);
+    }
+
+    @Override
+    public SnapFileFilter getProductFileFilter() {
+        String[] formatNames = getFormatNames();
+        String formatName = "";
+        if (formatNames.length > 0) {
+            formatName = formatNames[0];
+        }
+        return new SnapFileFilter(formatName, getDefaultFileExtensions(), getDescription(null));
+    }
+
+    /**
+     * Gets the default file extensions associated with each of the format names returned by the <code>{@link
+     * #getFormatNames}</code> method. <p>The string array returned shall always have the same length as the array
+     * returned by the <code>{@link #getFormatNames}</code> method. <p>The extensions returned in the string array shall
+     * always include a leading colon ('.') character, e.g. <code>".hdf"</code>
+     *
+     * @return the default file extensions for this product I/O plug-in, never <code>null</code>
+     */
+    @Override
+    public String[] getDefaultFileExtensions() {
+        // todo: return regular expression to clean up the extensions.
+        return new String[]{
+                DEFAULT_FILE_EXTENSION_L1B_MOD,
+                DEFAULT_FILE_EXTENSION_L1B_IMG,
+                DEFAULT_FILE_EXTENSION_L1B_DNB
+        };
+    }
+
+    /**
+     * Gets a short description of this plug-in. If the given locale is set to <code>null</code> the default locale is
+     * used.
+     * <p/>
+     * <p> In a GUI, the description returned could be used as tool-tip text.
+     *
+     * @param locale the local for the given decription string, if <code>null</code> the default locale is used
+     * @return a textual description of this product reader/writer
+     */
+    @Override
+    public String getDescription(Locale locale) {
+        return READER_DESCRIPTION;
+    }
+
+    /**
+     * Gets the names of the product formats handled by this product I/O plug-in.
+     *
+     * @return the names of the product formats handled by this product I/O plug-in, never <code>null</code>
+     */
+    @Override
+    public String[] getFormatNames() {
+        return new String[]{FORMAT_NAME};
+    }
+}
