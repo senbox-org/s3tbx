79eae8dab0b01d12fcab446cbec1486f8beba071
marpet <marco.peters@brockmann-consult.de>
2017-04-24 13:34:56 +0200

    [SIIITBX-147] Support Landsat data from new processed dataset "Collection 1"
    refactored landsat types

---
+++ s3tbx-landsat-reader/src/main/java/org/esa/s3tbx/dataio/landsat/geotiff/LandsatTypeInfo.java
@@ -0,0 +1,149 @@
+package org.esa.s3tbx.dataio.landsat.geotiff;
+
+/**
+ * @author Marco Peters
+ */
+class LandsatTypeInfo {
+
+    private static final String L4_FILENAME_REGEX = "LT4\\d{13}\\w{3}\\d{2}";
+    private static final String L5_FILENAME_REGEX = "LT5\\d{13}\\w{3}\\d{2}";
+    private static final String L7_FILENAME_REGEX = "LE7\\d{13}\\w{3}\\d{2}";
+    private static final String L8_FILENAME_REGEX = "L[OTC]8\\d{13}\\w{3}\\d{2}";
+    public static final String MSS_FILENAME_REGEX = "LM[1-5]\\d{13}\\w{3}\\d{2}";
+    public static final String L5LEGACY_FILENAME_REGEX_1 = "LT5\\d{13}\\w{3}\\d{2}";
+    public static final String L5LEGACY_FILENAME_REGEX_2 = "L5\\d{6}_\\d{11}";
+    public static final String L7LEGACY_FILENAME_REGEX_1 = "LE7\\d{13}\\w{3}\\d{2}";
+    public static final String L7LEGACY_FILENAME_REGEX_2 = "L7\\d{7}_\\d{11}";
+
+    private enum LandsatType {
+        LANDSAT_MSS {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                return filename.matches(MSS_FILENAME_REGEX + "_MTL" + getTxtExtension());
+            }
+        },
+        LANDSAT4 {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                if (filename.matches(L4_FILENAME_REGEX + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L4_FILENAME_REGEX + getCompressionExtension())) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        LANDSAT5 {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                if (filename.matches(L5_FILENAME_REGEX + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L5_FILENAME_REGEX + getCompressionExtension())) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        LANDSAT5_LEGACY {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                if (filename.matches(L5LEGACY_FILENAME_REGEX_1 + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L5LEGACY_FILENAME_REGEX_2 + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L5LEGACY_FILENAME_REGEX_1 + getCompressionExtension())) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        LANDSAT7 {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                if (filename.matches(L7_FILENAME_REGEX + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L7_FILENAME_REGEX + getCompressionExtension())) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        LANDSAT7_LEGACY {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                if (filename.matches(L7LEGACY_FILENAME_REGEX_1 + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L7LEGACY_FILENAME_REGEX_2 + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L7LEGACY_FILENAME_REGEX_1 + getCompressionExtension())) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        LANDSAT8 {
+            @Override
+            boolean matchesFileNamepattern(String filename) {
+                if (filename.matches(L8_FILENAME_REGEX + "_MTL" + getTxtExtension())) {
+                    return true;
+                } else if (filename.matches(L8_FILENAME_REGEX + getCompressionExtension())) {
+                    return true;
+                }
+                return false;
+            }
+        };
+
+        abstract boolean matchesFileNamepattern(String filename);
+    }
+
+
+    private LandsatTypeInfo() {
+    }
+
+    public static boolean isLandsat(String fileName) {
+        for (LandsatType type : LandsatType.values()) {
+            if (type.matchesFileNamepattern(fileName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isMss(String fileName) {
+        return LandsatType.LANDSAT_MSS.matchesFileNamepattern(fileName);
+    }
+
+    static boolean isLandsat4(String fileName) {
+        return LandsatType.LANDSAT4.matchesFileNamepattern(fileName);
+    }
+
+    static boolean isLandsat5Legacy(String fileName) {
+        return LandsatType.LANDSAT5_LEGACY.matchesFileNamepattern(fileName);
+    }
+
+    static boolean isLandsat5(String fileName) {
+        return LandsatType.LANDSAT5.matchesFileNamepattern(fileName);
+    }
+
+    static boolean isLandsat7Legacy(String fileName) {
+        return LandsatType.LANDSAT7_LEGACY.matchesFileNamepattern(fileName);
+    }
+
+    static boolean isLandsat7(String fileName) {
+        return LandsatType.LANDSAT7.matchesFileNamepattern(fileName);
+    }
+
+    static boolean isLandsat8(String fileName) {
+        return LandsatType.LANDSAT8.matchesFileNamepattern(fileName);
+    }
+
+
+    private static String getCompressionExtension() {
+        return "\\.(tar\\.gz|tgz|tar\\.bz|tbz|tar\\.bz2|tbz2|zip|ZIP)";
+    }
+
+    private static String getTxtExtension() {
+        return "\\.(txt|TXT)";
+    }
+
+}
