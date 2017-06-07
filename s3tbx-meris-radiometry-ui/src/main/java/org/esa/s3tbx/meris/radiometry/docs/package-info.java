9c4e9516c112a525181e4a0a6d43756bde8bbfda
marpet <marco.peters@brockmann-consult.de>
2017-06-07 09:19:37 +0200

    avoid duplicate position of helpset entries

--- s3tbx-meris-radiometry-ui/src/main/java/org/esa/s3tbx/meris/radiometry/docs/package-info.java
+++ s3tbx-meris-radiometry-ui/src/main/java/org/esa/s3tbx/meris/radiometry/docs/package-info.java
@@ -1,4 +1,4 @@
-@HelpSetRegistration(helpSet = "help.hs", position = 4020)
+@HelpSetRegistration(helpSet = "help.hs", position = 6000)
 package org.esa.s3tbx.meris.radiometry.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
--- s3tbx-rad2refl-ui/src/main/java/org/esa/s3tbx/processor/rad2refl/docs/package-info.java
+++ s3tbx-rad2refl-ui/src/main/java/org/esa/s3tbx/processor/rad2refl/docs/package-info.java
@@ -1,4 +1,4 @@
-@HelpSetRegistration(helpSet = "help.hs", position = 4420)
+@HelpSetRegistration(helpSet = "help.hs", position = 4520)
 package org.esa.s3tbx.processor.rad2refl.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
