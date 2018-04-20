ff8c5bb94ff215d221d565926db64b46ad3c8f44
Olaf Danne <olaf.danne@brockmann-consult.de>
2018-04-20 14:53:14 +0200

    Idepix OLCI: now uses L1b Land flag instead of reading SRTM mask; merged separate operators for classif over land and water into one Op

--- s3tbx-idepix/src/main/java/org/esa/s3tbx/idepix/algorithms/olci/OlciConstants.java
+++ s3tbx-idepix/src/main/java/org/esa/s3tbx/idepix/algorithms/olci/OlciConstants.java
@@ -9,8 +9,12 @@ public class OlciConstants {
 
     /* Level 1 Quality Flags Positions */
     public static final int L1_F_LAND = 31;
+    public static final int L1_F_COASTLINE = 30;
+//    public static final int L1_F_FRESH_INLAND_WATER = 29;
+    public static final int L1_F_BRIGHT = 27;
     public static final int L1_F_INVALID = 25;
     public static final int L1_F_GLINT = 22;
+
 
     public static final String OLCI_QUALITY_FLAGS_BAND_NAME = "quality_flags";
 }
