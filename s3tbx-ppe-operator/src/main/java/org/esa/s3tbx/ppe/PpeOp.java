e245b53aa40c7bd08a9af05c10be2d153aa80ddc
Soman2 <roman.shevchuk@brockmann-consult.de>
2018-09-24 11:14:20 +0200

    First version of Prompt Particle Event processor added.

---
+++ s3tbx-ppe-operator/src/main/java/org/esa/s3tbx/ppe/PpeOp.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2012 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+
+package org.esa.s3tbx.ppe;
+
+
+import com.bc.ceres.core.ProgressMonitor;
+import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.FlagCoding;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.gpf.Operator;
+import org.esa.snap.core.gpf.OperatorException;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.Tile;
+import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
+import org.esa.snap.core.gpf.annotations.SourceProduct;
+import org.esa.snap.core.gpf.annotations.TargetProduct;
+import org.esa.snap.core.util.ProductUtils;
+import org.esa.snap.core.util.SystemUtils;
+
+import java.awt.*;
+import java.util.Arrays;
+
+/**
+ *
+ *
+ */
+@OperatorMetadata(
+        alias = "PpeOperator",
+        version = "1.1",
+        category = "Optical/Preprocessing",
+        description = "",
+        authors = " ",
+        copyright = "(c) 2018 by Brockmann Consult GmbH")
+
+public class PpeOp extends Operator {
+
+    @SourceProduct(alias = "source", description = "The source product")
+    private Product sourceProduct;
+
+    @TargetProduct()
+    private Product targetProduct;
+
+    @Parameter(defaultValue = "0.7",
+            description = "Minimum threshold to differentiate with the neighboring pixels in [mW.m-2.sr-1.nm-1]")
+    private double cutOff;
+
+    @Parameter(defaultValue = "10",
+            description = "Multiplier of Median Absolute Deviation used for the threshold.")
+    private double numberOfMAD;
+
+
+    @Override
+    public void initialize() throws OperatorException {
+        createTargetProduct();
+    }
+
+    @Override
+    public void computeTile(Band targetBand, Tile targetTile, ProgressMonitor pm) throws OperatorException {
+        Rectangle targetRectangle = targetTile.getRectangle();
+        Tile sourceTile;
+        if (!targetBand.getName().toLowerCase().contains("_ppe_flag")) {
+            sourceTile = getSourceTile(sourceProduct.getRasterDataNode(targetBand.getName()), targetRectangle);
+        }
+        else {
+            sourceTile = getSourceTile(sourceProduct.getRasterDataNode(targetBand.getName().replace("_ppe_flag","")), targetRectangle);
+        }
+        for (int y = targetRectangle.y; y < targetRectangle.y + targetRectangle.height; y++) {
+            checkForCancellation();
+            for (int x = targetRectangle.x; x < targetRectangle.x + targetRectangle.width; x++) {
+                double pixel = sourceTile.getSampleDouble(x, y);
+                if ( pixel>0) {
+                    double[] pixelList = getPixelList(x, y, sourceTile);
+                    double median = getMedian(pixelList);
+                    double MAD = getMAD(pixelList);
+
+                    if (!targetBand.getName().toLowerCase().contains("_ppe_flag")) {
+                        setBandTile(x, y, median, MAD, sourceTile, targetTile);
+                    } else {
+                        setFlagBandTile(x, y, median, MAD, sourceTile, targetTile);
+                    }
+                }
+                else {
+                    targetTile.setSample(x, y, pixel);
+                }
+            }
+        }
+    }
+
+    private void createTargetProduct() {
+        targetProduct = new Product(sourceProduct.getName(), sourceProduct.getProductType(), sourceProduct.getSceneRasterWidth(), sourceProduct.getSceneRasterHeight());
+
+        for (Band band : sourceProduct.getBands()) {
+            if (band.getName().toLowerCase().contains("radiance") && (band.getSpectralWavelength()!=0.0f)){
+                ProductUtils.copyBand(band.getName(), sourceProduct, targetProduct, false);
+                if (!"mW.m-2.sr-1.nm-1".equals(band.getUnit())){
+                    SystemUtils.LOG.warning("The units of "+band.getName()+" are not mW.m-2.sr-1.nm-1. Changing cut-off parameter is suggested.");
+                }
+                Band ppeBand = new Band(band.getName()+"_ppe_flag", ProductData.TYPE_INT8,sourceProduct.getSceneRasterWidth(),sourceProduct.getSceneRasterHeight());
+                final FlagCoding flagCoding = new FlagCoding("ppe_applied");
+                flagCoding.setDescription("PPE proccesor flag");
+                flagCoding.addFlag("PPE applied",1,"PPE applied");
+                flagCoding.addFlag("PPE not applied",0,"PPE not applied");
+                ppeBand.setSampleCoding(flagCoding);
+                targetProduct.getFlagCodingGroup().add(flagCoding);
+                targetProduct.addBand(ppeBand);
+            }
+            else{
+                ProductUtils.copyBand(band.getName(), sourceProduct, targetProduct, true);
+
+            }
+        }
+
+        ProductUtils.copyMetadata(sourceProduct, targetProduct);
+        ProductUtils.copyTiePointGrids(sourceProduct, targetProduct);
+        ProductUtils.copyMasks(sourceProduct, targetProduct);
+        ProductUtils.copyFlagBands(sourceProduct, targetProduct, true);
+        ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
+    }
+
+    void setBandTile(int x, int y,double median,double MAD, Tile sourceTile, Tile targetTile){
+        double pixel = sourceTile.getSampleDouble(x, y);
+        if (Math.abs(pixel - median) > cutOff && Math.abs(pixel - median) > (numberOfMAD * MAD)) {
+            targetTile.setSample(x, y, median);
+        }
+        else{
+            targetTile.setSample(x, y, pixel);
+        }
+    }
+
+
+    void setFlagBandTile(int x, int y,double median,double MAD, Tile sourceTile, Tile targetTile){
+        double pixel = sourceTile.getSampleDouble(x, y);
+        if (Math.abs(pixel - median) > cutOff && Math.abs(pixel - median) > (numberOfMAD * MAD)) {
+            targetTile.setSample(x, y, 1);
+        }
+        else{
+            targetTile.setSample(x, y, 0);
+        }
+    }
+
+
+    static double[] getPixelList(int x, int y, Tile sourceTile) {
+        double[] pixelList = new double[5];
+        pixelList[0] = getPixelValue(sourceTile, x, y - 2);
+        pixelList[1] = getPixelValue(sourceTile, x, y - 1);
+        pixelList[2] = getPixelValue(sourceTile, x, y);
+        pixelList[3] = getPixelValue(sourceTile, x, y + 1);
+        pixelList[4] = getPixelValue(sourceTile, x, y + 2);
+        Arrays.sort(pixelList);
+        if (pixelList[0]<0){
+            throw new OperatorException("Radiance bands contain values lower than zero at x="+x+" y="+y);
+        }
+        return pixelList;
+    }
+
+     static Double getPixelValue(Tile tile, int x, int y) {
+        if ( (0 <= y) && (y < tile.getHeight())) {
+            return tile.getSampleDouble(x, y);
+        } else {
+            return 0d;
+        }
+    }
+
+    static Double getMedian(double[] listDoubles) {
+        Arrays.sort(listDoubles);
+        if (listDoubles[1]==0) {
+            return (listDoubles[3]);
+        } else if (listDoubles[0]==0) {
+            return ((listDoubles[2] + listDoubles[3]) / 2);
+        } else {
+            return (listDoubles[2]);
+        }
+    }
+
+    static Double getMAD(double[] listDoubles) {
+        Double median = getMedian(listDoubles);
+        double[] listMAD = new double[5];
+        for (int i = 0; i < 5; i++) {
+            if (listDoubles[i] != 0) {
+                listMAD[i] = Math.abs(listDoubles[i] - median);
+            } else {
+                listMAD[i] = -1;
+            }
+        }
+        Arrays.sort(listMAD);
+        if (listMAD[1]==-1) {
+            return listMAD[3];
+        }
+        else if (listMAD[0]==-1){
+            return (listMAD[2]+listMAD[3])/2;
+        }
+        else {
+            return listMAD[2];
+        }
+    }
+
+
+    public static class Spi extends OperatorSpi {
+        public Spi() {
+            super(PpeOp.class);
+        }
+    }
+}
+
---
+++ s3tbx-ppe-operator/src/test/java/org/esa/s3tbx/ppe/PpeOpTest.java
@@ -0,0 +1,149 @@
+package org.esa.s3tbx.ppe;
+
+import com.bc.ceres.glevel.MultiLevelImage;
+import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.datamodel.*;
+import org.esa.snap.core.gpf.Operator;
+import org.esa.snap.core.gpf.internal.TileImpl;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.Raster;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class PpeOpTest {
+
+    private static final float[] MERIS_WAVELENGTHS = new float[]{412, 442, 490, 510, 560, 620, 665, 681, 709, 754, 761, 779, 865, 885, 900};
+    private String TESTFILENAME ="ppetest.nc";
+
+    @Test
+    public void testPpeOp()  {
+        Operator ppeOp = new PpeOp();
+        ppeOp.setSourceProduct(createSourceProduct());
+        Product targetProduct = ppeOp.getTargetProduct();
+
+        assertEquals(MERIS_WAVELENGTHS.length*2+1,targetProduct.getNumBands());
+        assertTrue(targetProduct.containsBand("radiance3_ppe_flag"));
+        assertTrue(targetProduct.containsBand("radiance8_ppe_flag"));
+        assertTrue(targetProduct.containsBand("radiance8"));
+        assertTrue(targetProduct.containsBand("anotherBand"));
+        assertTrue(!targetProduct.containsBand("anotherBand_ppe_flag"));
+        assertTrue(targetProduct.getBand("radiance3_ppe_flag").isFlagBand());
+        FlagCoding flagCoding = targetProduct.getBand("radiance3_ppe_flag").getFlagCoding();
+        assertEquals(2, flagCoding.getFlagNames().length);
+    }
+
+    @Test
+    public void testGetMedian() {
+        double[] list = new double[] {1,2,1,1,1};
+        assertEquals (1,PpeOp.getMedian(list),1e-10);
+        double[] list2 = new double[] {0,0,1,4,2};
+        assertEquals (2,PpeOp.getMedian(list2),1e-10);
+        double[] list3 = new double[] {5,4,3,2,0};
+        assertEquals (3.5,PpeOp.getMedian(list3),1e-10);
+    }
+
+    @Test
+    public void testGetMAD(){
+        double[] list = new double[] {1,6,3,7,11};
+        assertEquals (3,PpeOp.getMAD(list),1e-10);
+        double[] list2 = new double[] {0,0,3,1,11};
+        assertEquals (2,PpeOp.getMAD(list2),1e-10);
+        double[] list3 = new double[] {8,0,103,20,21};
+        assertEquals (6.5,PpeOp.getMAD(list3),1e-10);
+
+    }
+    @Test
+    public void getPixelValueTest() throws IOException{
+        String testFilePath = PpeOpTest.class.getResource(TESTFILENAME).getFile();
+        Product sourceProduct = ProductIO.readProduct(testFilePath);
+        RasterDataNode rasterDataNode =sourceProduct.getRasterDataNode("Oa11_radiance");
+        Rectangle expectedRect = new Rectangle(0, 0, 20, 20);
+        MultiLevelImage image = rasterDataNode.getSourceImage();
+        Raster raster = image.getData(expectedRect);
+        TileImpl sourceTile = new TileImpl(rasterDataNode, raster);
+
+        Double testValue = PpeOp.getPixelValue(sourceTile,0,21);
+        assertEquals(0,testValue,1e-5);
+
+        Double testValue2 = PpeOp.getPixelValue(sourceTile,3,image.getMaxY()+1);
+        assertEquals(0,testValue2,1e-5);
+
+
+        Double testValue3 = PpeOp.getPixelValue(sourceTile,8,-1);
+        assertEquals(0,testValue3,1e-5);
+
+        Double testValue4 = PpeOp.getPixelValue(sourceTile,1,1);
+        assertEquals(10.36252,testValue4,1e-5);
+
+        Double testValue5 = PpeOp.getPixelValue(sourceTile,10,10);
+        assertEquals(10.47061,testValue5,1e-5);
+    }
+
+    @Test
+    public void getPixelListTest() throws IOException {
+        String testFilePath = PpeOpTest.class.getResource(TESTFILENAME).getFile();
+        Product sourceProduct = ProductIO.readProduct(testFilePath);
+
+        RasterDataNode rasterDataNode = sourceProduct.getRasterDataNode("Oa11_radiance");
+        Rectangle expectedRect = new Rectangle(0, 0, 20, 20);
+        MultiLevelImage image = rasterDataNode.getSourceImage();
+        Raster raster = image.getData(expectedRect);
+        TileImpl sourceTile = new TileImpl(rasterDataNode, raster);
+
+        double[] pixelList1 = PpeOp.getPixelList(0,  0,  sourceTile);
+        assertEquals(0,pixelList1[0],1e-5);
+        assertEquals(10.39630,pixelList1[2],1e-5);
+        assertEquals(10.58545,pixelList1[4],1e-5);
+
+        double[] pixelList2 = PpeOp.getPixelList(0,  19,  sourceTile);
+        assertEquals(0,pixelList2[0],1e-5);
+        assertEquals(0,pixelList2[1],1e-5);
+        assertEquals(10.22066,pixelList2[2],1e-5);
+    }
+
+
+    @Test
+    public void testWithProduct() throws IOException {
+        Operator ppeOp = new PpeOp();
+        String testFilePath = PpeOpTest.class.getResource(TESTFILENAME).getFile();
+        Product product = ProductIO.readProduct(testFilePath);
+        ppeOp.setSourceProduct(product);
+        ppeOp.setParameterDefaultValues();
+        Product result = ppeOp.getTargetProduct();
+        result.getBand("Oa10_radiance").readRasterDataFully();
+        result.getBand("Oa10_radiance_ppe_flag").readRasterDataFully();
+
+        assertEquals(113, result.getNumBands());
+        assertEquals(20, result.getSceneRasterHeight());
+        assertEquals(20, result.getSceneRasterWidth());
+        assertEquals(13.20930, result.getBand("Oa10_radiance").getPixelDouble(0, 3), 1e-5);
+        assertEquals(13.20930, result.getBand("Oa10_radiance").getPixelDouble(0, 4), 1e-5);
+        assertEquals(13.10102, result.getBand("Oa10_radiance").getPixelDouble(7, 9), 1e-5);
+        assertEquals(1, result.getBand("Oa10_radiance_ppe_flag").getPixelDouble(0, 3), 1e-5);
+        assertEquals(0, result.getBand("Oa10_radiance_ppe_flag").getPixelDouble(0, 4), 1e-5);
+        assertEquals(1, result.getBand("Oa10_radiance_ppe_flag").getPixelDouble(6, 9), 1e-5);
+        assertEquals(1, result.getBand("Oa10_radiance_ppe_flag").getPixelDouble(18, 6), 1e-5);
+        assertEquals(0, result.getBand("Oa10_radiance_ppe_flag").getPixelDouble(19, 19), 1e-5);
+
+    }
+
+    private Product createSourceProduct() {
+        Product product = new Product("OWT_Input", "REFLEC", 10, 10);
+        for (int i = 0; i < MERIS_WAVELENGTHS.length; i++) {
+            Band reflecBand = product.addBand("radiance" + (i + 1), ProductData.TYPE_FLOAT32);
+            reflecBand.setSpectralWavelength(MERIS_WAVELENGTHS[i]);
+            reflecBand.setSpectralBandwidth(10);
+            reflecBand.setUnit("mW.m-2.sr-1.nm-1");
+        }
+        Band anotherBand = product.addBand("anotherBand", ProductData.TYPE_FLOAT32);
+        anotherBand.setSpectralWavelength(10000);
+        anotherBand.setSpectralBandwidth(10);
+
+        return product;
+    }
+}
