e76a72c980919bb4fb954f4529603ba334cc7501
Omar Barrilero <omar.barrilero@c-s.fr>
2019-03-17 18:48:48 +0100

    Add specific color iterators

--- s3tbx-landsat-reader/src/main/java/org/esa/s3tbx/dataio/landsat/geotiff/AbstractLandsatQA.java
+++ s3tbx-landsat-reader/src/main/java/org/esa/s3tbx/dataio/landsat/geotiff/AbstractLandsatQA.java
@@ -72,40 +72,4 @@ abstract class AbstractLandsatQA implements LandsatQA {
                                             0.5));
         return masks;
     }
-
-    /*protected static class ColorIterator {
-
-        static ArrayList<Color> colors;
-        static Iterator<Color> colorIterator;
-
-        static {
-            colors = new ArrayList<>();
-            colors.add(Color.red);
-            colors.add(Color.red.darker());
-            colors.add(Color.red.darker().darker());
-            colors.add(Color.blue);
-            colors.add(Color.blue.darker());
-            colors.add(Color.blue.darker().darker());
-            colors.add(Color.green);
-            colors.add(Color.green.darker());
-            colors.add(Color.green.darker().darker());
-            colors.add(Color.yellow);
-            colors.add(Color.yellow.darker());
-            colors.add(Color.yellow.darker().darker());
-            colors.add(Color.magenta);
-            colors.add(Color.magenta.darker());
-            colors.add(Color.magenta.darker().darker());
-            colors.add(Color.pink);
-            colors.add(Color.pink.darker());
-            colors.add(Color.pink.darker().darker());
-            colorIterator = colors.iterator();
-        }
-
-        static Color next() {
-            if (!colorIterator.hasNext()) {
-                colorIterator = colors.iterator();
-            }
-            return colorIterator.next();
-        }
-    }*/
 }
