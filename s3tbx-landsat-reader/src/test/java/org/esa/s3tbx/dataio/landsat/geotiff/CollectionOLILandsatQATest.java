01467147483325cad9a0a8000f14dfa7b4efa40c
marpet <marco.peters@brockmann-consult.de>
2019-03-18 08:33:12 +0100

    removed temporary test
    (cherry picked from commit 5fd8415d32bf809b7e08b14f35bfe8d0da5774ce)

--- s3tbx-landsat-reader/src/test/java/org/esa/s3tbx/dataio/landsat/geotiff/CollectionOLILandsatQATest.java
+++ s3tbx-landsat-reader/src/test/java/org/esa/s3tbx/dataio/landsat/geotiff/CollectionOLILandsatQATest.java
@@ -1,15 +1,9 @@
 package org.esa.s3tbx.dataio.landsat.geotiff;
 
-import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Mask;
-import org.esa.snap.core.datamodel.Product;
-import org.junit.Assume;
 import org.junit.Test;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.io.File;
-import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -22,23 +16,6 @@ public class CollectionOLILandsatQATest {
         CollectionOLILandsatQA landsatQA = new CollectionOLILandsatQA();
         java.util.List<Mask> masks = landsatQA.createMasks(new Dimension(1, 1));
         assertEquals(masks.size(),19);
-    }
-
-    @Test
-    public void testConsistentMaskColors() throws IOException {
-        File file1 = new File("D:\\Produits_S2\\sensors_platforms\\LANDSAT\\LANDSAT_8_OLI_TIRS\\geotiff\\LC81970222013122LGN01\\LC81970222013122LGN01_MTL.txt");
-        File file2 = new File("D:\\Produits_S2\\sensors_platforms\\LANDSAT\\LANDSAT_8_OLI_TIRS\\geotiff\\LC81960222013195LGN00\\LC81960222013195LGN00_MTL.txt");
-
-        Assume.assumeTrue(file1.exists());
-        Assume.assumeTrue(file2.exists());
-
-        Product product1 = ProductIO.readProduct(file1);
-        assertEquals(Color.RED, product1.getMaskGroup().get("designated_fill").getImageColor());
-
-
-        Product product2 = ProductIO.readProduct(file2);
-        assertEquals(Color.RED, product2.getMaskGroup().get("designated_fill").getImageColor());
-
     }
 
 }
