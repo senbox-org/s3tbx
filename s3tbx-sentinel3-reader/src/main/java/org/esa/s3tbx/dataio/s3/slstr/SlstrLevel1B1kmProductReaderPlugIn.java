670fbf0a028f45b705834f76e0d2f22ab2ed0b16
Marco Peters <marco.peters@brockmann-consult.de>
2019-05-03 11:16:05 +0200

    [SIIITBX-245] Only the generic S3 reader should be intended for SLSTR data (#34)

--- s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/slstr/SlstrLevel1B1kmProductReaderPlugIn.java
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/slstr/SlstrLevel1B1kmProductReaderPlugIn.java
@@ -1,6 +1,7 @@
 package org.esa.s3tbx.dataio.s3.slstr;
 
 import org.esa.s3tbx.dataio.s3.Sentinel3ProductReaderPlugIn;
+import org.esa.snap.core.dataio.DecodeQualification;
 
 /**
  * @author Tonio Fincke
@@ -12,6 +13,15 @@ public class SlstrLevel1B1kmProductReaderPlugIn extends Sentinel3ProductReaderPl
     public SlstrLevel1B1kmProductReaderPlugIn() {
         super(format_name, "Sentinel-3 SLSTR L1B products in 1km resolution",
               "(S3.?_SL_1_RBT_.*(.SEN3)?)", "xfdumanifest", "L1c_Manifest", ".xml");
+    }
+
+    @Override
+    public DecodeQualification getDecodeQualification(Object input) {
+        if(isInputValid(input)) {
+            return DecodeQualification.SUITABLE;
+        }else {
+            return DecodeQualification.UNABLE;
+        }
     }
 
 }
