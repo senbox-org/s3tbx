13654c9adb521c56482a7145e8db7f350a32fd56
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-06-03 15:08:55 +0100

    improved tests (#37)

--- s3tbx-slstr-pdu-stitching/src/test/java/org/esa/s3tbx/slstr/pdu/stitching/manifest/ManifestMergerTest.java
+++ s3tbx-slstr-pdu-stitching/src/test/java/org/esa/s3tbx/slstr/pdu/stitching/manifest/ManifestMergerTest.java
@@ -1,17 +1,23 @@
 package org.esa.s3tbx.slstr.pdu.stitching.manifest;
 
 
+import org.esa.s3tbx.slstr.pdu.stitching.PDUStitchingException;
 import org.esa.s3tbx.slstr.pdu.stitching.TestUtils;
 import org.esa.snap.core.util.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * @author Tonio Fincke
@@ -45,67 +51,22 @@ public class ManifestMergerTest {
         }
     }
 
-//    @Test
-//    public void testMergeManifests_OneFile() throws IOException, ParserConfigurationException, TransformerException, PDUStitchingException {
-//        final File inputManifest = getManifestFile(TestUtils.FIRST_FILE_NAME);
-//        final Date now = Calendar.getInstance().getTime();
-//        final File productDir = new File(ManifestMergerTest.class.getResource("").getFile());
-//        final File manifestFile = manifestMerger.createMergedManifest(new File[]{inputManifest}, now, productDir, 5000);
-//        //todo assert something
-//    }
-//
-//    @Test
-//    public void testMergeManifests_MultipleFiles() throws IOException, ParserConfigurationException, TransformerException, PDUStitchingException {
-//        final Date now = Calendar.getInstance().getTime();
-//        final File productDir = new File(ManifestMergerTest.class.getResource("").getFile());
-//        final File manifestFile = manifestMerger.createMergedManifest(getManifestFiles(), now, productDir, 5000);
-////        final Document manifest = manifestMerger.mergeManifests(getManifestFiles(), now, productDir);
-////        final Transformer transformer = TransformerFactory.newInstance().newTransformer();
-////        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-////        transformer.setOutputProperty(OutputKeys.STANDALONE, "no");
-////        final DOMSource manifestSource = new DOMSource(manifest);
-////        final File manifestFile = new File(targetDirectory, "xfdumanifest.xml");
-////        final StreamResult streamResult = new StreamResult(manifestFile);
-////        transformer.transform(manifestSource, streamResult);
-//        //todo assert something
-//    }
-//
-//    @Test
-//    @Ignore()
-//    public void testMergeSentinelSafeProcessingNodes() throws IOException, ParserConfigurationException, SAXException {
-//        //todo make this run -> own elementmerger?
-//        List<Node> fromParents = new ArrayList<>();
-//        fromParents.add(ManifestTestUtils.createNode(
-//                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-//                        "          <sentinel-safe:processing name=\"DataProcessing\" outputLevel=\"1\" start=\"2015-02-17T18:35:19.139217Z\" stop=\"2015-02-17T18:58:46.896371Z\">\n" +
-//                        "          </sentinel-safe:processing>"));
-//        fromParents.add(ManifestTestUtils.createNode(
-//                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-//
-//                        "                    <sentinel-safe:processing name=\"DataProcessing\" outputLevel=\"1\" start=\"2015-02-17T18:35:18.291550Z\" stop=\"2015-02-17T18:58:57.569852Z\">\n" +
-//                        "          </sentinel-safe:processing>"));
-//        fromParents.add(ManifestTestUtils.createNode(
-//                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-//
-//                        "          <sentinel-safe:processing name=\"DataProcessing\" outputLevel=\"1\" start=\"2015-02-17T18:35:25.916879Z\" stop=\"2015-02-17T18:58:50.564464Z\">\n" +
-//                        "          </sentinel-safe:processing>"));
-//        final Element manifestElement = manifest.createElement("sentinel-safe:processing");
-//        manifest.appendChild(manifestElement);
-//
-////        manifestMerger.mergeChildNodes(fromParents, manifestElement);
-////        manifestMerger.mergeChildNodes(fromParents, manifestElement, manifest);
-//
-//        final NamedNodeMap manifestElementAttributes = manifestElement.getFirstChild().getAttributes();
-//        assertEquals(4, manifestElementAttributes.getLength());
-//        assertNotNull(manifestElementAttributes.getNamedItem("name"));
-//        assertEquals("DataProcessing", manifestElementAttributes.getNamedItem("name").getNodeValue());
-//        assertNotNull(manifestElementAttributes.getNamedItem("outputLevel"));
-//        assertEquals("1", manifestElementAttributes.getNamedItem("outputLevel").getNodeValue());
-//        assertNotNull(manifestElementAttributes.getNamedItem("start").getNodeValue());
-//        assertEquals("2015-02-17T18:35:18.291550Z", manifestElementAttributes.getNamedItem("start").getNodeValue());
-//        assertNotNull(manifestElementAttributes.getNamedItem("stop").getNodeValue());
-//        assertEquals("2015-02-17T18:58:57.569852Z", manifestElementAttributes.getNamedItem("stop").getNodeValue());
-//    }
+    @Test
+    public void testMergeManifests_OneFile() throws IOException, ParserConfigurationException, TransformerException, PDUStitchingException {
+        final File inputManifest = getManifestFile(TestUtils.FIRST_FILE_NAME);
+        final Date now = Calendar.getInstance().getTime();
+        final File productDir = new File(ManifestMergerTest.class.getResource("").getFile());
+        final File manifestFile = manifestMerger.createMergedManifest(new File[]{inputManifest}, now, productDir, 5000);
+        assertTrue(manifestFile.exists());
+    }
+
+    @Test
+    public void testMergeManifests_MultipleFiles() throws IOException, ParserConfigurationException, TransformerException, PDUStitchingException {
+        final Date now = Calendar.getInstance().getTime();
+        final File productDir = new File(ManifestMergerTest.class.getResource("").getFile());
+        final File manifestFile = manifestMerger.createMergedManifest(getManifestFiles(), now, productDir, 5000);
+        assertTrue(manifestFile.exists());
+    }
 
     private static File[] getManifestFiles() {
         return new File[]{getManifestFile(TestUtils.FIRST_FILE_NAME),
