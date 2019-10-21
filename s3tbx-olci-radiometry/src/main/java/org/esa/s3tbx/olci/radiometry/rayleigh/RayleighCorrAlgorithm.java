fa992b98cd8e7dea8bbccd49a14107430a7c737e
dagmarM <Dagmar.Mueller@brockmann-consult.de>
2019-10-21 17:51:40 +0200

    - re-initialising of trans709 needs to happen inside the loop over all pixels. - bWVTile (not bWVRefTile) needs to be greater zero. - Introducing central wavelengths calculated from SRF for OLCI A. Needs implementation for OLCI B as well. - nCO2 formula was wrong. - getRhoRayleigh(): for debugging, calculate the Rayleigh reflectance and write it to the target product. - getRayleighThicknessBodhaineTest(): checks sigma, tau_R against tabulated values from Bodhaine paper. (needs to switch the pressure calculation in getRayleighOpticalThickness for that)

--- s3tbx-olci-radiometry/src/main/java/org/esa/s3tbx/olci/radiometry/rayleigh/RayleighCorrAlgorithm.java
+++ s3tbx-olci-radiometry/src/main/java/org/esa/s3tbx/olci/radiometry/rayleigh/RayleighCorrAlgorithm.java
@@ -37,9 +37,9 @@ public class RayleighCorrAlgorithm {
         // trans709 = H2O_COR_POLY[0] + (H2O_COR_POLY[1] + (H2O_COR_POLY[2] + H2O_COR_POLY[3] * X2) * X2) * X2
         // when X2 = 1
         // trans709 = 0.3832989 + ( 1.6527957+ (-1.5635101+ 0.5311913*1)*1)*1
-        double trans709 = 1.0037757999999999;
         for (int i = 0; i < bWVTile.length; i++) {
-            if (bWVRefTile[i] > 0) {
+            double trans709 = 1.0037757999999999;
+            if (bWVTile[i] > 0) {
                 double X2 = bWVTile[i] / bWVRefTile[i];
                 trans709 = H2O_COR_POLY[0] + (H2O_COR_POLY[1] + (H2O_COR_POLY[2] + H2O_COR_POLY[3] * X2) * X2) * X2;
             }
@@ -56,6 +56,14 @@ public class RayleighCorrAlgorithm {
             // now use 'true' instead of central wavelengths instead
             // CB/GK 20170721, following https://earth.esa.int/documents/247904/685211/Sentinel-2+MSI+Spectral+Responses/
             return getCrossSection(S2Utils.getS2TrueWavelengths());
+        } else if (sensor != null && sensor == Sensor.OLCI){
+            //todo: these wavelengths belong to S3A - needs also S3B !
+            double[] waveLength = new double[] {400.3031914558257, 411.8452957525705, 442.9625672120682, 490.4930356580268, 510.46748124416945,
+                    560.4502797598624, 620.4092905501666, 665.2744162328253, 674.0251490485472, 681.5706005756095,
+                    709.1148593849875, 754.1813240203888, 761.7260948029898, 764.8247093465473, 767.9174355161354,
+                    779.2567595815481, 865.4296340763456, 884.3082558969855, 899.3107685704568, 938.9730748611009,
+                    1015.7990909091901};
+            return getCrossSection(waveLength);
         } else {
             double[] waveLength = new double[numBands];
             for (int i = 0; i < numBands; i++) {
@@ -76,13 +84,12 @@ public class RayleighCorrAlgorithm {
      */
     public double[] getCrossSection(double[] lambdas) {
         double n_ratio = 1 + 0.54 * (RayleighConstants.CO2 - 0.0003);
-        double molecularDen = Math.pow(RayleighConstants.Molecular_cm3, 2);
+        double molecularDen = RayleighConstants.Molecular_cm3;
         double[] sigma = new double[lambdas.length];
 
         for (int i = 0; i < lambdas.length; i++) {
-
             double lambdamm = lambdas[i] / 1000.0;
-            double lambdaWLcm = lambdamm / 10000.0;
+            double lambdaWLcm = lambdas[i] / 1.E7;
 
             // [1], eq. (5):
             double F_N2 = 1.034 + 0.000317 / Math.pow(lambdamm, 2);
@@ -96,10 +103,10 @@ public class RayleighCorrAlgorithm {
             double F_air = (78.084 * F_N2 + 20.946 * F_O2 + 0.934 * 1 + RayleighConstants.C_CO2 * 1.15) /
                     (78.084 + 20.946 + 0.934 + RayleighConstants.C_CO2);
 
-            double nCO2 = n_ratio * (1 + n_1_300);
+            double nCO2 = n_ratio * n_1_300 + 1.;
             // [1], eq. (22):
             sigma[i] = 24 * Math.pow(Math.PI, 3) * Math.pow((Math.pow(nCO2, 2) - 1), 2) / (Math.pow(lambdaWLcm, 4) *
-                    molecularDen * Math.pow((Math.pow(nCO2, 2) + 2), 2)) * F_air;
+                    Math.pow(molecularDen, 2) * Math.pow((Math.pow(nCO2, 2) + 2), 2)) * F_air;
         }
         return sigma;
     }
@@ -175,6 +182,7 @@ public class RayleighCorrAlgorithm {
             double[] rho_Rm = getFourierSeries(taurVal, massAir, cosOZARad, cosSZARad, interpolateValues, fourierSeries);
 
             double rho_R = rho_Rm[0] + 2.0 * rho_Rm[1] * Math.cos(aziDiff) + 2.0 * rho_Rm[2] * Math.cos(2.0 * aziDiff);
+
             // polynomial coefficients tpoly0, tpoly1 and tpoly2 from MERIS LUT
 
             double tRs = ((2.0 / 3.0 + cosSZARad) + (2.0 / 3.0 - cosSZARad) * Math.exp(-taurVal / cosSZARad)) / (4.0 / 3.0 + taurVal);
@@ -194,6 +202,55 @@ public class RayleighCorrAlgorithm {
             rho_BRR[index] = (rho_toaR * sphericalFactor);
         }
         return rho_BRR;
+    }
+
+    public double[] getRhoRayleigh(RayleighAux rayleighAux, double[] rayleighOpticalThickness, double[] corrOzoneRefl) {
+        //for debugging.
+        final double[] airMasses = rayleighAux.getAirMass();
+        final double[] aziDiffs = rayleighAux.getAziDifferent();
+        final double[] cosSZARads = rayleighAux.getCosSZARads();
+        final double[] cosOZARads = rayleighAux.getCosOZARads();
+
+        final Map<Integer, double[]> fourier = rayleighAux.getFourier();
+        final Map<Integer, List<double[]>> interpolation = rayleighAux.getInterpolation();
+        final int length = cosOZARads.length;
+
+        final double[] rho_R = new double[length];
+
+        for (int index = 0; index < length; index++) {
+
+            double corrOzone = corrOzoneRefl[index];
+            if (Double.isNaN(corrOzone)) {
+                rho_R[index] = RayleighConstants.INVALID_VALUE;
+                continue;
+            }
+            if (corrOzone <= 0) {
+                rho_R[index] = 0.0;
+                continue;
+            }
+            double taurVal = rayleighOpticalThickness[index];
+            if (Double.isNaN(taurVal)) {
+                rho_R[index] = taurVal;
+                continue;
+            }
+            double aziDiff = aziDiffs[index];
+            double massAir = airMasses[index];
+
+            double cosOZARad = cosOZARads[index];
+            double cosSZARad = cosSZARads[index];
+
+            List<double[]> interpolateValues = interpolation.get(index);
+            if (interpolateValues == null || interpolateValues.size() == 0) {
+                // this might happen if we are out of range of RayleighAux data (e.g. SZA > 80deg)
+                // --> set to NaN (CB, 20171026)
+                rho_R[index] = RayleighConstants.INVALID_VALUE;
+                continue;
+            }
+            double[] fourierSeries = fourier.get(index);
+            double[] rho_Rm = getFourierSeries(taurVal, massAir, cosOZARad, cosSZARad, interpolateValues, fourierSeries);
+            rho_R[index] = rho_Rm[0] + 2.0 * rho_Rm[1] * Math.cos(aziDiff) + 2.0 * rho_Rm[2] * Math.cos(2.0 * aziDiff);
+        }
+        return rho_R;
     }
 
     public double[] getFourierSeries(double rayleighOpticalThickness, double massAir, double cosOZARad, double cosSZARad, List<double[]> interpolateValues, double[] fourierSeriesCof) {
@@ -266,6 +323,9 @@ public class RayleighCorrAlgorithm {
         final int crossSectionSigmaIndex = getCrossSectionSigmaIndex(sourceBandIndex, targetBandName);
         double sigma = crossSectionSigma[crossSectionSigmaIndex];
 
+        //test
+        //getRayleighThicknessBodhaineTest();
+
         double rayleighOpticalThickness[] = new double[altitudes.length];
         for (int i = 0; i < altitudes.length; i++) {
             rayleighOpticalThickness[i] = getRayleighOpticalThickness(sigma, seaLevels[i], altitudes[i], latitudes[i]);
@@ -274,15 +334,54 @@ public class RayleighCorrAlgorithm {
         return rayleighOpticalThickness;
     }
 
+
+    public void getRayleighThicknessBodhaineTest() {
+        double[] seaLevels = new double[] {1013.5, 680.};
+        double[] altitudes = new double[] {0., 3400.};
+        double[] latitudes = new double[] {45., 19.5338};
+        double[] wavelength = new double[] {400., 405., 410.};
+
+        double[] sigma = getCrossSection(wavelength);
+        double[] bodhaine_sigma = new double[] {1.67380E-26, 1.58950E-26, 1.51050E-26};
+
+        for(int i=0; i<bodhaine_sigma.length; i++){
+            System.out.println("sigma");
+            System.out.println(wavelength[i]);
+            System.out.println((sigma[i]-bodhaine_sigma[i])/bodhaine_sigma[i]*100.);
+        } // sigma error is fine! implementation is fine.
+
+        //example 1 at sealevel:
+        double[] bodhaine_taur1 = new double[] {3.6022E-01, 3.4207E-01, 3.2506E-01};
+        //example 2 at 3400m:
+        double[] bodhaine_taur2 = new double[] {2.4243E-01, 2.3022E-01, 2.1877E-01};
+
+        for (int i = 0; i < altitudes.length; i++) {
+            double[] rayleighOpticalThickness = new double[sigma.length];
+            for(int j=0; j<sigma.length; j++) {
+                rayleighOpticalThickness[j] = getRayleighOpticalThickness(sigma[j], seaLevels[i], altitudes[i], latitudes[i]);
+                if( altitudes[i]==0.){
+                    System.out.println((rayleighOpticalThickness[j]-bodhaine_taur1[j])/bodhaine_taur1[j]*100.);
+                }
+                else if( altitudes[i]>0.){
+                    System.out.println((rayleighOpticalThickness[j]-bodhaine_taur2[j])/bodhaine_taur2[j]*100.);
+                }
+            }
+        }
+
+    }
+
+
     public double getRayleighOpticalThickness(double sigma, double seaLevelPressure, double altitude, double latitude) {
         double P = seaLevelPressure * Math.pow((1.0 - 0.0065 * altitude / 288.15), 5.255) * 1000;
+        //for getRayleighThicknessBodhaineTest() use only the following line (pressure is already at altitude):
+        //double P = seaLevelPressure * 1000.;
         double latRad = Math.toRadians(latitude);
         double cos2LatRad = Math.cos(2 * latRad);
         double g0 = 980.616 * (1 - 0.0026373 * cos2LatRad + 0.0000059 * Math.pow(cos2LatRad, 2));
         double effectiveMassWeightAltitude = 0.73737 * altitude + 5517.56;
 
-        double g = g0 - (0.0003085462 + 0.000000227 * cos2LatRad) * effectiveMassWeightAltitude +
-                (0.00000000007254 + 0.0000000000001 * cos2LatRad) * Math.pow(effectiveMassWeightAltitude, 2) -
+        double g = g0 - (3.085462E-4 + 2.27E-7  * cos2LatRad) * effectiveMassWeightAltitude +
+                (7.254E-11 + 1E-13 * cos2LatRad) * Math.pow(effectiveMassWeightAltitude, 2) -
                 (1.517E-17 + 6E-20 * cos2LatRad) * Math.pow(effectiveMassWeightAltitude, 3);
 
         double factor = (P * RayleighConstants.AVOGADRO_NUMBER) / (RayleighConstants.MEAN_MOLECULAR_WEIGHT_C02 * g);
