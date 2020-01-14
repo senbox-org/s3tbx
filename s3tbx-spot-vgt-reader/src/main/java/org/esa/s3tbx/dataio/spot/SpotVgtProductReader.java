369160adf921ab77088bd571d3afa9b4a8b72a5e
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2020-01-14 16:51:56 +0100

    updated netcdf dependencies
    (cherry picked from commit e85be89cc31987d523385b4908d0300b5fb393c0)

--- s3tbx-spot-vgt-reader/src/main/java/org/esa/s3tbx/dataio/spot/SpotVgtProductReader.java
+++ s3tbx-spot-vgt-reader/src/main/java/org/esa/s3tbx/dataio/spot/SpotVgtProductReader.java
@@ -213,7 +213,7 @@ public class SpotVgtProductReader extends AbstractProductReader {
         Variable variable = findPixelDataVariable(netcdfFile);
         if (isPotentialPixelDataVariable(variable)) {
             DataType netCdfDataType = variable.getDataType();
-            int bandDataType = convertNetcdfTypeToProductDataType(netCdfDataType, variable.isUnsigned());
+            int bandDataType = convertNetcdfTypeToProductDataType(netCdfDataType, variable.getDataType().isUnsigned());
             if (bandDataType != ProductData.TYPE_UNDEFINED) {
                 String bandName = getBandName(hdfFileName);
                 BandInfo bandInfo = getBandInfo(bandName);
