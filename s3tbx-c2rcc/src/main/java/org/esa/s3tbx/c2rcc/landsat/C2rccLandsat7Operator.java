c5f6d355beb426736eabe420b63ae40789e7d32e
Marco Peters <marco.peters@brockmann-consult.de>
2020-02-21 15:20:24 +0100

    [SIIITBX-330] Provide better names vor conversion parameter for TSM and CHL
    (cherry picked from commit 9c7996f)

--- s3tbx-c2rcc/src/main/java/org/esa/s3tbx/c2rcc/landsat/C2rccLandsat7Operator.java
+++ s3tbx-c2rcc/src/main/java/org/esa/s3tbx/c2rcc/landsat/C2rccLandsat7Operator.java
@@ -47,8 +47,43 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.esa.s3tbx.c2rcc.C2rccCommons.*;
-import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.*;
+import static org.esa.s3tbx.c2rcc.C2rccCommons.addBand;
+import static org.esa.s3tbx.c2rcc.C2rccCommons.addVirtualBand;
+import static org.esa.s3tbx.c2rcc.C2rccCommons.fetchOzone;
+import static org.esa.s3tbx.c2rcc.C2rccCommons.fetchSurfacePressure;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.DEFAULT_WAVELENGTH;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_ADET_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_ADET_AT_MIN;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_AGELB_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_AGELB_AT_MIN;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_APIG_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_APIG_AT_MIN;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_BPART_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_BPART_AT_MIN;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_BWIT_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_BWIT_AT_MIN;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_CLOUD;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_IOP_OOR;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_KD489_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_KD489_OOR;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_KDMIN_AT_MAX;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_KDMIN_OOR;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_RHOW_OOR;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_RHOW_OOS;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_RTOSA_OOR;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_RTOSA_OOS;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.FLAG_INDEX_VALID_PE;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_iop_rw;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_iop_unciop;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_iop_uncsumiop_unckd;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rtosa_aann;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rtosa_rpath;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rtosa_rw;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rtosa_trans;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rw_iop;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rw_kd;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.IDX_rw_rwnorm;
+import static org.esa.s3tbx.c2rcc.landsat.C2rccLandsat7Algorithm.Result;
 
 // todo (nf) - Add Thullier solar fluxes as default values to C2RCC operator (https://github.com/bcdev/s3tbx-c2rcc/issues/1)
 // todo (nf) - Add flags band and check for OOR of inputs and outputs of the NNs (https://github.com/bcdev/s3tbx-c2rcc/issues/2)
@@ -195,16 +230,16 @@ public class C2rccLandsat7Operator extends PixelOperator implements C2rccConfigu
             description = "Used as fallback if elevation could not be taken from GETASSE30 DEM.")
     private double elevation;
 
-    @Parameter(defaultValue = "1.72", description = "Conversion factor bpart. (TSM = bpart * TSMfakBpart + bwit * TSMfakBwit)", label = "TSM factor bpart")
+    @Parameter(alias = "TSMfac", defaultValue = "1.72", description = "TSM factor (TSM = TSMfac * iop_btot^TSMexp).", label = "TSM factor")
     private double TSMfakBpart;
 
-    @Parameter(defaultValue = "3.1", description = "Conversion factor bwit. (TSM = bpart * TSMfakBpart + bwit * TSMfakBwit)", label = "TSM factor bwit")
+    @Parameter(alias = "TSMexp", defaultValue = "3.1", description = "TSM exponent (TSM = TSMfac * iop_btot^TSMexp).", label = "TSM exponent")
     private double TSMfakBwit;
 
-    @Parameter(defaultValue = "1.04", description = "Chlorophyll exponent ( CHL = iop-apig^CHLexp * CHLfak ) ", label = "CHL exponent")
+    @Parameter(alias = "CHLexp", defaultValue = "1.04", description = "Chlorophyll exponent ( CHL = iop_apig^CHLexp * CHLfac).", label = "CHL exponent")
     private double CHLexp;
 
-    @Parameter(defaultValue = "21.0", description = "Chlorophyll factor ( CHL = iop-apig^CHLexp * CHLfak ) ", label = "CHL factor")
+    @Parameter(alias = "CHLfac", defaultValue = "21.0", description = "Chlorophyll factor ( CHL = iop_apig^CHLexp * CHLfac).", label = "CHL factor")
     private double CHLfak;
 
     @Parameter(defaultValue = "0.05", description = "Threshold for out of scope of nn training dataset flag for gas corrected top-of-atmosphere reflectances.",
