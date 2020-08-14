1c21890d557888d9d0e94b641d62d53566e65975
Marco Peters <marco.peters@brockmann-consult.de>
2020-08-14 10:46:51 +0200

    added Supresswarning for enum

--- s3tbx-flhmci/src/main/java/org/esa/s3tbx/processor/flh_mci/Presets.java
+++ s3tbx-flhmci/src/main/java/org/esa/s3tbx/processor/flh_mci/Presets.java
@@ -1,5 +1,6 @@
 package org.esa.s3tbx.processor.flh_mci;
 
+@SuppressWarnings("unused")
 public enum Presets {
     NONE("None", "", "", "", "", "", ""),
     MERIS_L1B_MCI("MERIS L1b MCI", "radiance_8", "radiance_10", "radiance_9", "MCI", "MCI_slope",
@@ -9,11 +10,11 @@ public enum Presets {
     MERIS_L2_MCI("MERIS L2 MCI", "reflec_8", "reflec_10", "reflec_9", "MCI", "MCI_slope",
                  "l2_flags.WATER"),
     OLCI_L1B_MCI("OLCI L1b MCI", "Oa10_radiance", "Oa12_radiance", "Oa11_radiance", "MCI", "MCI_slope",
-                  "(!quality_flags.land || quality_flags.fresh_inland_water) && ! quality_flags.invalid"),
+                 "(!quality_flags.land || quality_flags.fresh_inland_water) && ! quality_flags.invalid"),
     OLCI_L2_FLH("OLCI L2 FLH", "Oa08_reflectance", "Oa11_reflectance", "Oa10_reflectance", "FLH", "FLH_slope",
-                 "WQSF_lsb.WATER || WQSF_lsb.INLAND_WATER"),
+                "WQSF_lsb.WATER || WQSF_lsb.INLAND_WATER"),
     OLCI_L2_MCI("OLCI L2 MCI", "Oa10_reflectance", "Oa12_reflectance", "Oa11_reflectance", "MCI", "MCI_slope",
-                 "WQSF_lsb.WATER || WQSF_lsb.INLAND_WATER");
+                "WQSF_lsb.WATER || WQSF_lsb.INLAND_WATER");
 
     public final String label;
     public final String lowerBaselineBandName;
@@ -24,7 +25,7 @@ public enum Presets {
     public final String maskExpression;
 
     Presets(String label, String upperBaselineBandName, String lowerBaselineBandName,
-                    String signalBandName, String lineHeightBandName, String slopeBandName, String maskExpression) {
+            String signalBandName, String lineHeightBandName, String slopeBandName, String maskExpression) {
         this.label = label;
         this.upperBaselineBandName = upperBaselineBandName;
         this.lowerBaselineBandName = lowerBaselineBandName;
