76655d0044887deec22db14bae26a505080003fa
flicka07 <helgaelisa.ganz@gmail.com>
2020-10-06 20:52:35 +0200

    integration of cams auxillary data

--- s3tbx-c2rcc/src/main/java/org/esa/s3tbx/c2rcc/viirs/C2rccViirsOperator.java
+++ s3tbx-c2rcc/src/main/java/org/esa/s3tbx/c2rcc/viirs/C2rccViirsOperator.java
@@ -101,28 +101,28 @@ public class C2rccViirsOperator extends PixelOperator implements C2rccConfigurab
     private Product sourceProduct;
 
     @SourceProduct(description = "The first product providing ozone values for ozone interpolation. " +
-            "Use either the TOMSOMI and NCEP products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
+            "Use either the TOMSOMI and NCEP products or CAMS products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
             optional = true,
-            label = "Ozone interpolation start product (TOMSOMI)")
-    private Product tomsomiStartProduct;
+            label = "Ozone interpolation start product (TOMSOMI or CAMS)")
+    private Product ozoneStartProduct;
 
     @SourceProduct(description = "The second product providing ozone values for ozone interpolation. " +
-            "Use either the TOMSOMI and NCEP products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
+            "Use either the TOMSOMI and NCEP products or CAMS products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
             optional = true,
-            label = "Ozone interpolation end product (TOMSOMI)")
-    private Product tomsomiEndProduct;
+            label = "Ozone interpolation end product (TOMSOMI or CAMS)")
+    private Product ozoneEndProduct;
 
     @SourceProduct(description = "The first product providing air pressure values for pressure interpolation. " +
-            "Use either the TOMSOMI and NCEP products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
+            "Use either the TOMSOMI and NCEP products or CAMS products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
             optional = true,
-            label = "Air pressure interpolation start product (NCEP)")
-    private Product ncepStartProduct;
+            label = "Air pressure interpolation start product (NCEP or CAMS)")
+    private Product pressureStartProduct;
 
     @SourceProduct(description = "The second product providing air pressure values for pressure interpolation. " +
-            "Use either the TOMSOMI and NCEP products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
+            "Use either the TOMSOMI and NCEP products or CAMS products or the atmosphericAuxdataPath to as source for ozone and air pressure.",
             optional = true,
-            label = "Air pressure interpolation end product (NCEP)")
-    private Product ncepEndProduct;
+            label = "Air pressure interpolation end product (NCEP or CAMS)")
+    private Product pressureEndProduct;
 
     @Parameter(label = "Valid-pixel expression",
             defaultValue = "!(l2_flags.LAND || rhot_862 > 0.25)",
@@ -146,8 +146,8 @@ public class C2rccViirsOperator extends PixelOperator implements C2rccConfigurab
             description = "The surface air pressure at sea level if not provided by auxiliary data")
     private double press;
 
-    @Parameter(description = "Path to the atmospheric auxiliary data directory. Use either this or tomsomiStartProduct, " +
-            "tomsomiEndProduct, ncepStartProduct and ncepEndProduct to use ozone and air pressure aux data " +
+    @Parameter(description = "Path to the atmospheric auxiliary data directory. Use either this or ozoneStartProduct, " +
+            "ozoneEndProduct, pressureStartProduct and pressureEndProduct to use ozone and air pressure aux data " +
             "for calculations. If the auxiliary data needed for interpolation not available in this " +
             "path, the data will automatically downloaded.")
     private String atmosphericAuxDataPath;
@@ -164,6 +164,7 @@ public class C2rccViirsOperator extends PixelOperator implements C2rccConfigurab
     private C2rccViirsAlgorithm algorithm;
     private AtmosphericAuxdata atmosphericAuxdata;
     private TimeCoding timeCoding;
+    private ProductData.UTC sourceTime;
 
     @Override
     public void setAtmosphericAuxDataPath(String atmosphericAuxDataPath) {
@@ -171,23 +172,23 @@ public class C2rccViirsOperator extends PixelOperator implements C2rccConfigurab
     }
 
     @Override
-    public void setTomsomiStartProduct(Product tomsomiStartProduct) {
-        this.tomsomiStartProduct = tomsomiStartProduct;
+    public void setOzoneStartProduct(Product ozoneStartProduct) {
+        this.ozoneStartProduct = ozoneStartProduct;
     }
 
     @Override
-    public void setTomsomiEndProduct(Product tomsomiEndProduct) {
-        this.tomsomiEndProduct = tomsomiEndProduct;
+    public void setOzoneEndProduct(Product ozoneEndProduct) {
+        this.ozoneEndProduct = ozoneEndProduct;
     }
 
     @Override
-    public void setNcepStartProduct(Product ncepStartProduct) {
-        this.ncepStartProduct = ncepStartProduct;
+    public void setPressureStartProduct(Product pressureStartProduct) {
+        this.pressureStartProduct = pressureStartProduct;
     }
 
     @Override
-    public void setNcepEndProduct(Product ncepEndProduct) {
-        this.ncepEndProduct = ncepEndProduct;
+    public void setPressureEndProduct(Product pressureEndProduct) {
+        this.pressureEndProduct = pressureEndProduct;
     }
 
     @Override
@@ -355,6 +356,7 @@ public class C2rccViirsOperator extends PixelOperator implements C2rccConfigurab
             algorithm.setSalinity(salinity);
             pm.worked(1);
             pm.setSubTaskName("Initialising atmospheric auxiliary data");
+            sourceTime = sourceProduct.getStartTime();
             initAtmosphericAuxdata();
             pm.worked(1);
         } catch (IOException e) {
@@ -394,8 +396,9 @@ public class C2rccViirsOperator extends PixelOperator implements C2rccConfigurab
         auxdataBuilder.setOzone(ozone);
         auxdataBuilder.setSurfacePressure(press);
         auxdataBuilder.useAtmosphericAuxDataPath(atmosphericAuxDataPath);
-        auxdataBuilder.useTomsomiProducts(tomsomiStartProduct, tomsomiEndProduct);
-        auxdataBuilder.useNcepProducts(ncepStartProduct, ncepEndProduct);
+        auxdataBuilder.useTomsomiCamsProducts(ozoneStartProduct, ozoneEndProduct);
+        auxdataBuilder.useNcepCamsProducts(pressureStartProduct, pressureEndProduct);
+        auxdataBuilder.setSourceTime(sourceTime);
         try {
             atmosphericAuxdata = auxdataBuilder.create();
         } catch (Exception e) {
--- s3tbx-c2rcc/src/test/java/org/esa/s3tbx/c2rcc/ancillary/AtmosphericAuxdataStaticTest.java
+++ s3tbx-c2rcc/src/test/java/org/esa/s3tbx/c2rcc/ancillary/AtmosphericAuxdataStaticTest.java
@@ -2,7 +2,10 @@ package org.esa.s3tbx.c2rcc.ancillary;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import static org.junit.Assert.*;
 
@@ -24,7 +27,7 @@ public class AtmosphericAuxdataStaticTest {
     }
 
     private void verifyCalendar(Calendar calendar, int doy, int year, int month, int dayOfMonth, int hour) {
-        assertEquals(doy,calendar.get(Calendar.DAY_OF_YEAR));
+        assertEquals(doy, calendar.get(Calendar.DAY_OF_YEAR));
         assertEquals(year, calendar.get(Calendar.YEAR));
         assertEquals(month, calendar.get(Calendar.MONTH));
         assertEquals(dayOfMonth, calendar.get(Calendar.DAY_OF_MONTH));
@@ -34,4 +37,38 @@ public class AtmosphericAuxdataStaticTest {
         assertEquals(0, calendar.get(Calendar.MILLISECOND));
     }
 
+    @Test
+    public void testHoursSince1900ToCalendar() throws Exception {
+        int hoursSince1900_start = 1054812;
+        long year_start = 2020;
+        long month_start = 4; //Calendar month are from 0 to 11
+        long dayOfMonth_start = 1;
+        long hour_start = 12;
+
+        int hoursSince1900_end = 1055532;
+        long year_end = 2020;
+        long month_end = 4; //Calendar month are from 0 to 11
+        long dayOfMonth_end = 31;
+        long hour_end = 12;
+
+        Calendar calendar_start = AtmosphericAuxdataStatic.createCalendarByHoursSince1900(hoursSince1900_start);
+
+        assertEquals(year_start, calendar_start.get(Calendar.YEAR));
+        assertEquals(month_start, calendar_start.get(Calendar.MONTH));
+        assertEquals(dayOfMonth_start, calendar_start.get(Calendar.DAY_OF_MONTH));
+        assertEquals(hour_start, calendar_start.get(Calendar.HOUR_OF_DAY));
+        assertEquals(0, calendar_start.get(Calendar.MINUTE));
+        assertEquals(0, calendar_start.get(Calendar.SECOND));
+        assertEquals(0, calendar_start.get(Calendar.MILLISECOND));
+
+        Calendar calendar_end = AtmosphericAuxdataStatic.createCalendarByHoursSince1900(hoursSince1900_end);
+
+        assertEquals(year_end, calendar_end.get(Calendar.YEAR));
+        assertEquals(month_end, calendar_end.get(Calendar.MONTH));
+        assertEquals(dayOfMonth_end, calendar_end.get(Calendar.DAY_OF_MONTH));
+        assertEquals(hour_end, calendar_end.get(Calendar.HOUR_OF_DAY));
+        assertEquals(0, calendar_end.get(Calendar.MINUTE));
+        assertEquals(0, calendar_end.get(Calendar.SECOND));
+        assertEquals(0, calendar_end.get(Calendar.MILLISECOND));
+    }
 }
