1092dd59bb2350580137d89721165ebee38f140d
Olaf Danne <olaf.danne@brockmann-consult.de>
2021-03-17 17:27:35 +0100

    added operator incl. help documentation for MERIS 4RP --> 3RP in s3tbx-sentinel3-reader

---
+++ s3tbx-sentinel3-reader/src/test/java/org/esa/s3tbx/dataio/s3/meris/reprocessing/Meris4thTo3rdReprocesingOpTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package org.esa.s3tbx.dataio.s3.meris.reprocessing;
+
+import org.esa.snap.core.gpf.GPF;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.OperatorSpiRegistry;
+import org.junit.Test;
+
+import java.util.Locale;
+
+import static org.junit.Assert.*;
+
+public class Meris4thTo3rdReprocesingOpTest {
+
+    static {
+        Locale.setDefault(Locale.UK);
+    }
+
+    @Test
+    public void testOperatorSpiIsLoaded() {
+        OperatorSpiRegistry registry = GPF.getDefaultInstance().getOperatorSpiRegistry();
+        OperatorSpi operatorSpi = registry.getOperatorSpi("Meris.Reprocessing.Adapter.4To3");
+        assertNotNull(operatorSpi);
+        assertEquals("Meris.Reprocessing.Adapter.4To3", operatorSpi.getOperatorAlias());
+        assertNotNull(operatorSpi.getOperatorDescriptor());
+        assertSame(operatorSpi.getOperatorClass(), operatorSpi.getOperatorDescriptor().getOperatorClass());
+    }
+
+
+    @Test
+    public void testMerisReproAdapterExists() {
+        Meris3rd4thReprocessingAdapter adapter = new Meris3rd4thReprocessingAdapter();
+        assertNotNull(adapter);
+    }
+}
