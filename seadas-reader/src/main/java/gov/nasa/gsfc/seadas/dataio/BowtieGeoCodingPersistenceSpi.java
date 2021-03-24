c59531d635430daf185ccc654b51a6ebd2c9c012
sabine <sabine.embacher@brockmann-consult.de>
2021-03-24 10:09:40 +0100

    replacement of DimapPersistable by new Persistence framework for ComponentGeoCoding, BowtieGeoCoding and PodGeoCoding

---
+++ seadas-reader/src/main/java/gov/nasa/gsfc/seadas/dataio/BowtieGeoCodingPersistenceSpi.java
@@ -0,0 +1,44 @@
+/*
+ *
+ * Copyright (c) 2021.  Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
+package gov.nasa.gsfc.seadas.dataio;
+
+import org.esa.snap.core.dataio.persistence.Item;
+import org.esa.snap.core.dataio.persistence.PersistenceConverter;
+import org.esa.snap.core.dataio.persistence.PersistenceSpi;
+
+public class BowtieGeoCodingPersistenceSpi implements PersistenceSpi {
+
+    public static final BowtieGeoCodingPersistenceConverter CONVERTER = new BowtieGeoCodingPersistenceConverter();
+
+    @Override
+    public boolean canDecode(Item item) {
+        return CONVERTER.canDecode(item);
+    }
+
+    @Override
+    public boolean canEncode(Object object) {
+        return object instanceof BowtieTiePointGeoCoding
+               || object instanceof BowtiePixelGeoCoding;
+    }
+
+    @Override
+    public PersistenceConverter createConverter() {
+        return CONVERTER;
+    }
+}
