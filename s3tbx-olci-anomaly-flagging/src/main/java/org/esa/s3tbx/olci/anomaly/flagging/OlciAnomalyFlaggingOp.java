29774f86b34e2a778301652d916fae65ca5a9c5e
tom <tom.block@brockmann-consult.de>
2021-04-09 15:37:55 +0200

    intermediate

--- s3tbx-olci-anomaly-flagging/src/main/java/org/esa/s3tbx/olci/anomaly/flagging/OlciAnomalyFlaggingOp.java
+++ s3tbx-olci-anomaly-flagging/src/main/java/org/esa/s3tbx/olci/anomaly/flagging/OlciAnomalyFlaggingOp.java
@@ -18,14 +18,19 @@ package org.esa.s3tbx.olci.anomaly.flagging;
 
 import com.bc.ceres.core.ProgressMonitor;
 import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.FlagCoding;
 import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.datamodel.TiePointGrid;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.OperatorSpi;
 import org.esa.snap.core.gpf.Tile;
 import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
 import org.esa.snap.core.gpf.annotations.SourceProduct;
 import org.esa.snap.core.gpf.annotations.TargetProduct;
+import org.esa.snap.core.util.ProductUtils;
 
 @OperatorMetadata(alias = "OlciAnomalyFlagging",
         version = "1.0",
@@ -35,6 +40,8 @@ import org.esa.snap.core.gpf.annotations.TargetProduct;
         description = "Adds a flagging band indicating saturated pixels and altitude data overflows")
 public class OlciAnomalyFlaggingOp extends Operator {
 
+    private static final String SUFFIX = "_ANOM_FLAG";
+
     @SourceProduct(description = "OLCI L1b or fully compatible product.",
             label = "OLCI L1b product")
     private Product l1bProduct;
@@ -42,24 +49,10 @@ public class OlciAnomalyFlaggingOp extends Operator {
     @TargetProduct
     private Product targetProduct;
 
-    @Override
-    public void initialize() throws OperatorException {
-        // validate input
-        // - solar flux
-        // - sza
-        // - wavelength
-
-        // create output product
-        // - copy all inputs
-        // - add anomaly-flags band
-
-
-    }
-
-    @Override
-    public void computeTile(Band targetBand, Tile targetTile, ProgressMonitor pm) throws OperatorException {
-
-    }
+    @Parameter(defaultValue = "false",
+            description = "If set to true, the operator adds two bands containing the maximal spectral slope and the band index where the peak is observed.",
+            label = "Write spectral slope information")
+    private boolean writeSlopeInformation;
 
     // package access for testing only tb 2021-04-08
     static double toReflectance(double radiance, double invSolarFlux, double invCosSZA) {
@@ -70,6 +63,70 @@ public class OlciAnomalyFlaggingOp extends Operator {
         checkRadianceBands(input, 2, 12);
         checkRadianceBands(input, 16, 18);
         checkRadianceBands(input, 21, 21);
+
+        checkSimpleIndexedBands(input, "solar_flux_band_", 2, 12);
+        checkSimpleIndexedBands(input, "solar_flux_band_", 16, 18);
+        checkSimpleIndexedBands(input, "solar_flux_band_", 21, 21);
+
+        checkSimpleIndexedBands(input, "lambda0_band_", 2, 12);
+        checkSimpleIndexedBands(input, "lambda0_band_", 16, 18);
+        checkSimpleIndexedBands(input, "lambda0_band_", 21, 21);
+
+        final Band altitude = input.getBand("altitude");
+        if (altitude == null) {
+            throw new OperatorException("Band 'altitude' missing.");
+        }
+
+        final TiePointGrid sza = input.getTiePointGrid("SZA");
+        if (sza == null) {
+            throw new OperatorException("Tie point grid 'SZA' missing.");
+        }
+    }
+
+    static Product createOutputProduct(Product input, boolean writeSlopeInformation) {
+        final String inputProductType = input.getProductType();
+        final String inputName = input.getName();
+        final Product outputProduct = new Product(inputName + SUFFIX,
+                                                  inputProductType + SUFFIX,
+                                                  input.getSceneRasterWidth(),
+                                                  input.getSceneRasterHeight());
+
+        outputProduct.setDescription("OLCI anomaly flagged L1b");
+        outputProduct.setStartTime(input.getStartTime());
+        outputProduct.setEndTime(input.getEndTime());
+
+        final Band[] inputBands = input.getBands();
+        for (final Band inputBand : inputBands) {
+            ProductUtils.copyBand(inputBand.getName(), input, outputProduct, true);
+        }
+
+        ProductUtils.copyFlagCodings(input, outputProduct);
+        ProductUtils.copyTiePointGrids(input, outputProduct);
+        ProductUtils.copyGeoCoding(input, outputProduct);
+        ProductUtils.copyMetadata(input, outputProduct);
+
+        final Band anomalyFlags = outputProduct.addBand("anomaly_flags", ProductData.TYPE_INT8);
+        anomalyFlags.setDescription("Flags indicating OLCI data anomalies");
+
+        final FlagCoding flagCoding = new FlagCoding("anomaly_flags");
+        flagCoding.addFlag("ANOM_SPECTRAL_MEASURE", 1, "Anomalous spectral sample due to saturation of single microbands");
+        flagCoding.addFlag("ALT_OUT_OF_RANGE", 2, "Altitude values are out of nominal data range");
+        anomalyFlags.setSampleCoding(flagCoding);
+
+        if (writeSlopeInformation) {
+            final Band maxSpectralSlope = outputProduct.addBand("max_spectral_slope", ProductData.TYPE_FLOAT32);
+            maxSpectralSlope.setNoDataValue(Float.NaN);
+            maxSpectralSlope.setNoDataValueUsed(true);
+            maxSpectralSlope.setUnit("1/nm");
+            maxSpectralSlope.setDescription("Absolute value of maximal spectral slope for bands 1-12, 16-18, 21");
+
+            final Band maxSlopeBandIndex = outputProduct.addBand("max_slope_band_index", ProductData.TYPE_INT8);
+            maxSlopeBandIndex.setNoDataValue(-1);
+            maxSlopeBandIndex.setNoDataValueUsed(true);
+            maxSlopeBandIndex.setDescription("Band index where the maximal slope is detected");
+        }
+
+        return outputProduct;
     }
 
     private static void checkRadianceBands(Product input, int lower, int upper) {
@@ -79,6 +136,28 @@ public class OlciAnomalyFlaggingOp extends Operator {
                 throw new OperatorException("Input variable '" + variableName + "' missing.");
             }
         }
+    }
+
+    private static void checkSimpleIndexedBands(Product input, String prefix, int lower, int upper) {
+        for (int i = lower; i <= upper; i++) {
+            final String variableName = prefix + Integer.toString(i);
+            if (!input.containsBand(variableName)) {
+                throw new OperatorException("Input variable '" + variableName + "' missing.");
+            }
+        }
+    }
+
+    @Override
+    public void initialize() throws OperatorException {
+        validateInputProduct(l1bProduct);
+
+        targetProduct = createOutputProduct(l1bProduct, writeSlopeInformation);
+        setTargetProduct(targetProduct);
+    }
+
+    @Override
+    public void computeTile(Band targetBand, Tile targetTile, ProgressMonitor pm) throws OperatorException {
+
     }
 
     public static class Spi extends OperatorSpi {
--- s3tbx-olci-anomaly-flagging/src/test/java/org/esa/s3tbx/olci/anomaly/flagging/OlciAnomalyFlaggingOpTest.java
+++ s3tbx-olci-anomaly-flagging/src/test/java/org/esa/s3tbx/olci/anomaly/flagging/OlciAnomalyFlaggingOpTest.java
@@ -17,16 +17,21 @@
 package org.esa.s3tbx.olci.anomaly.flagging;
 
 import org.esa.snap.core.datamodel.Band;
-import org.esa.snap.core.datamodel.MetadataAttribute;
-import org.esa.snap.core.datamodel.MetadataElement;
+import org.esa.snap.core.datamodel.FlagCoding;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.datamodel.TiePointGrid;
+import org.esa.snap.core.gpf.OperatorException;
 import org.junit.Test;
 
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class OlciAnomalyFlaggingOpTest {
 
@@ -52,8 +57,158 @@ public class OlciAnomalyFlaggingOpTest {
         OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
     }
 
+    @Test
+    public void testValidateInputProduct_missingRadiance() {
+        Product testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("Oa03_radiance"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+
+        testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("Oa17_radiance"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+    }
+
+    @Test
+    public void testValidateInputProduct_missingSolarFlux() {
+        Product testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("solar_flux_band_4"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+
+        testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("solar_flux_band_18"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+    }
+
+    @Test
+    public void testValidateInputProduct_missingSZA() {
+        Product testProduct = createTestProduct();
+        testProduct.removeTiePointGrid(testProduct.getTiePointGrid("SZA"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+    }
+
+    @Test
+    public void testValidateInputProduct_missingWavelength() {
+        Product testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("lambda0_band_5"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+
+        testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("lambda0_band_21"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+    }
+
+    @Test
+    public void testValidateInputProduct_missingAltitude() {
+        Product testProduct = createTestProduct();
+        testProduct.removeBand(testProduct.getBand("altitude"));
+
+        try {
+            OlciAnomalyFlaggingOp.validateInputProduct(testProduct);
+            fail("OperatorException expected");
+        } catch (OperatorException expected) {
+        }
+    }
+
+    @Test
+    public void testCreateOutputProduct() {
+        final Product testProduct = createTestProduct();
+
+        final Product outputProduct = OlciAnomalyFlaggingOp.createOutputProduct(testProduct, false);
+        assertEquals("test_type_ANOM_FLAG", outputProduct.getProductType());
+        assertEquals("test_me_ANOM_FLAG", outputProduct.getName());
+        assertEquals(3, outputProduct.getSceneRasterWidth());
+        assertEquals(5, outputProduct.getSceneRasterHeight());
+
+        assertEquals("OLCI anomaly flagged L1b", outputProduct.getDescription());
+        assertEquals("24-JAN-2021 18:50:51.000000", outputProduct.getStartTime().format());
+        assertEquals("25-JAN-2021 22:40:31.000000", outputProduct.getEndTime().format());
+
+        for (int i = 1; i < 22; i++) {
+            assertNotNull(outputProduct.getBand("Oa" + String.format("%02d", i) + "_radiance"));
+            assertNotNull(outputProduct.getBand("solar_flux_band_" + Integer.toString(i)));
+            assertNotNull(outputProduct.getBand("lambda0_band_" + Integer.toString(i)));
+        }
+
+        assertNotNull(outputProduct.getTiePointGrid("SZA"));
+
+        final Band anomalyFlags = outputProduct.getBand("anomaly_flags");
+        assertEquals(ProductData.TYPE_INT8, anomalyFlags.getDataType());
+        assertFalse(anomalyFlags.isNoDataValueUsed());
+        assertNull(anomalyFlags.getUnit());
+        assertEquals(0, anomalyFlags.getSpectralWavelength(), 1e-8);
+        assertEquals(0, anomalyFlags.getSpectralBandwidth(), 1e-8);
+        assertEquals("Flags indicating OLCI data anomalies", anomalyFlags.getDescription());
+
+        final FlagCoding flagCoding = anomalyFlags.getFlagCoding();
+        assertEquals(2, flagCoding.getFlagNames().length);
+        assertEquals(1, flagCoding.getFlagMask("ANOM_SPECTRAL_MEASURE"));
+        assertEquals(2, flagCoding.getFlagMask("ALT_OUT_OF_RANGE"));
+    }
+
+    @Test
+    public void testCreateOutputProduct_withSlopeBands() {
+        final Product testProduct = createTestProduct();
+
+        final Product outputProduct = OlciAnomalyFlaggingOp.createOutputProduct(testProduct, true);
+
+        final Band maxSpectralSlope = outputProduct.getBand("max_spectral_slope");
+        assertEquals(ProductData.TYPE_FLOAT32, maxSpectralSlope.getDataType());
+        assertEquals(Float.NaN, maxSpectralSlope.getNoDataValue(), 1e-8);
+        assertTrue(maxSpectralSlope.isNoDataValueUsed());
+        assertEquals("1/nm", maxSpectralSlope.getUnit());
+        assertEquals(0, maxSpectralSlope.getSpectralWavelength(), 1e-8);
+        assertEquals(0, maxSpectralSlope.getSpectralBandwidth(), 1e-8);
+        assertEquals("Absolute value of maximal spectral slope for bands 1-12, 16-18, 21", maxSpectralSlope.getDescription());
+
+        final Band bandIndex = outputProduct.getBand("max_slope_band_index");
+        assertEquals(ProductData.TYPE_INT8, bandIndex.getDataType());
+        assertEquals(-1, bandIndex.getNoDataValue(), 1e-8);
+        assertTrue(bandIndex.isNoDataValueUsed());
+        assertNull(bandIndex.getUnit());
+        assertEquals(0, bandIndex.getSpectralWavelength(), 1e-8);
+        assertEquals(0, bandIndex.getSpectralBandwidth(), 1e-8);
+        assertEquals("Band index where the maximal slope is detected", bandIndex.getDescription());
+    }
+
     private Product createTestProduct() {
         final Product product = new Product("test_me", "test_type", 3, 5);
+        product.setStartTime(ProductData.UTC.create(new Date(1611514251000L), 0));
+        product.setEndTime(ProductData.UTC.create(new Date(1611614431000L), 0));
 
         for (int i = 1; i < 22; i++) {
             final Band band = product.addBand("Oa" + String.format("%02d", i) + "_radiance", ProductData.TYPE_FLOAT32);
@@ -61,7 +216,15 @@ public class OlciAnomalyFlaggingOpTest {
             band.setSpectralWavelength(12.5f * i);
             band.setSpectralBandwidth(0.8f * i);
             band.setDescription("whatever");
+
+            product.addBand("solar_flux_band_" + Integer.toString(i), ProductData.TYPE_FLOAT32);
+            product.addBand("lambda0_band_" + Integer.toString(i), ProductData.TYPE_FLOAT32);
         }
+
+        product.addBand("altitude", ProductData.TYPE_FLOAT32);
+
+        final TiePointGrid sza = new TiePointGrid("SZA", 2, 2, 0, 0, 5, 5, new float[]{1, 2, 3, 4});
+        product.addTiePointGrid(sza);
 
         return product;
     }
