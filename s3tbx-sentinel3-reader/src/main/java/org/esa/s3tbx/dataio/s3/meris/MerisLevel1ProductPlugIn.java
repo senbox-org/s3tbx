fc709616e3c9af9774e96372865e601284a32300
Marco Peters <marco.peters@brockmann-consult.de>
2021-06-01 16:14:28 +0200

    reverted changes

--- s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/meris/MerisLevel1ProductPlugIn.java
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/meris/MerisLevel1ProductPlugIn.java
@@ -11,13 +11,14 @@ import org.esa.snap.core.datamodel.RGBImageProfileManager;
 public class MerisLevel1ProductPlugIn extends Sentinel3ProductReaderPlugIn {
 
     private static final String format_name = "MER_L1_S3";
+
     static {
         registerRGBProfiles();
     }
 
     public MerisLevel1ProductPlugIn() {
         super(format_name, "MERIS Level 1 in Sentinel-3 product format",
-                "EN.*_(F|R)R(G|P|S).*", "xfdumanifest", "L1c_Manifest", ".xml");
+              "EN.*_(F|R)R(G|P|S).*", "xfdumanifest", "L1c_Manifest", ".xml");
     }
 
     @Override
@@ -28,30 +29,30 @@ public class MerisLevel1ProductPlugIn extends Sentinel3ProductReaderPlugIn {
     private static void registerRGBProfiles() {
         RGBImageProfileManager manager = RGBImageProfileManager.getInstance();
         manager.addProfile(new RGBImageProfile("MERIS4 L1b - Tristimulus",
-                new String[]{
-                        "log(1.0 + 0.35 * M02_radiance + 0.60 * M05_radiance + M06_radiance + 0.13 * M07_radiance)",
-                        "log(1.0 + 0.21 * M03_radiance + 0.50 * M04_radiance + M05_radiance + 0.38 * M06_radiance)",
-                        "log(1.0 + 0.21 * M01_radiance + 1.75 * M02_radiance + 0.47 * M03_radiance + 0.16 * M04_radiance)"
-                },
-                new String[]{
-                        "*ME_1*",
-                        "ENV_ME_1_*",
-                        "",
-                }
+                                               new String[]{
+                                                       "log(1.0 + 0.35 * M02_radiance + 0.60 * M05_radiance + M06_radiance + 0.13 * M07_radiance)",
+                                                       "log(1.0 + 0.21 * M03_radiance + 0.50 * M04_radiance + M05_radiance + 0.38 * M06_radiance)",
+                                                       "log(1.0 + 0.21 * M01_radiance + 1.75 * M02_radiance + 0.47 * M03_radiance + 0.16 * M04_radiance)"
+                                               },
+                                               new String[]{
+                                                       "*ME_1*",
+                                                       "ENV_ME_1_*",
+                                                       "",
+                                               }
         ));
         manager.addProfile(new RGBImageProfile("MERIS4 L1b - 13,5,1",
-                new String[]{
-                        "M13_radiance",
-                        "M05_radiance",
-                        "M01_radiance"
-                }
+                                               new String[]{
+                                                       "M13_radiance",
+                                                       "M05_radiance",
+                                                       "M01_radiance"
+                                               }
         ));
         manager.addProfile(new RGBImageProfile("MERIS4 L1b - 13,4,2",
-                new String[]{
-                        "M13_radiance",
-                        "M04_radiance",
-                        "M02_radiance"
-                }
+                                               new String[]{
+                                                       "M13_radiance",
+                                                       "M04_radiance",
+                                                       "M02_radiance"
+                                               }
         ));
     }
 }
