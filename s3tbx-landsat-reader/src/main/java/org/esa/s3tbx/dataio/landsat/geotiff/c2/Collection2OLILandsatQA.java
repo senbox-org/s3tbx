f23e74538a09d7f6c0cfad13edad3d77da8ffca3
Marco Peters <marco.peters@brockmann-consult.de>
2022-05-05 11:56:32 +0200

    [SIIITBX-416] Clear flag is not considered for Landsat Collection 2 OLI data
    (cherry picked from commit a1c1e5573aceebfcfeb49abc6d1457da08a58024)

--- s3tbx-landsat-reader/src/main/java/org/esa/s3tbx/dataio/landsat/geotiff/c2/Collection2OLILandsatQA.java
+++ s3tbx-landsat-reader/src/main/java/org/esa/s3tbx/dataio/landsat/geotiff/c2/Collection2OLILandsatQA.java
@@ -20,59 +20,61 @@ public class Collection2OLILandsatQA extends AbstractLandsatQA {
 
     private final static Map<String, List<FlagCodingArgs>> codings = new HashMap<String, List<FlagCodingArgs>>() {{
         put("FILE_NAME_QUALITY_L1_PIXEL",
-                new ArrayList<FlagCodingArgs>() {{
-                    int position = 1;
-                    add(new FlagCodingArgs("designated_fill", position, "Designated Fill"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("dillated_cloud", position, "Dillated Cloud"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cirrus", position, "Cirrus"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cloud", position, "Cloud"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cloud_shadow", position, "Cloud Shadow"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("snow", position, "Snow/Ice Cover"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("water", position, "Water"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cloud_confidence_one", position, "Cloud confidence bit one"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cloud_confidence_two", position, "Cloud confidence bit two"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cloud_shadow_confidence_one", position, "Cloud shadow confidence bit one"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cloud_shadow_confidence_two", position, "Cloud shadow confidence bit two"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("snow_ice_confidence_one", position, "Snow/ice confidence bit one"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("snow_ice_confidence_two", position, "Snow/ice confidence bit two"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cirrus_confidence_one", position, "Cirrus confidence bit one"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("cirrus_confidence_two", position, "Cirrus confidence bit two"));
-                }});
+            new ArrayList<FlagCodingArgs>() {{
+                int position = 1; // 0
+                add(new FlagCodingArgs("designated_fill", position, "Designated Fill"));
+                position <<= 1; // 1
+                add(new FlagCodingArgs("dillated_cloud", position, "Dillated Cloud"));
+                position <<= 1; // 2
+                add(new FlagCodingArgs("cirrus", position, "Cirrus"));
+                position <<= 1; // 3
+                add(new FlagCodingArgs("cloud", position, "Cloud"));
+                position <<= 1; // 4
+                add(new FlagCodingArgs("cloud_shadow", position, "Cloud Shadow"));
+                position <<= 1; // 5
+                add(new FlagCodingArgs("snow", position, "Snow/Ice Cover"));
+                position <<= 1; // 6
+                add(new FlagCodingArgs("clear", position, "Clear"));
+                position <<= 1; // 7
+                add(new FlagCodingArgs("water", position, "Water"));
+                position <<= 1; // 8
+                add(new FlagCodingArgs("cloud_confidence_one", position, "Cloud confidence bit one"));
+                position <<= 1; // 9
+                add(new FlagCodingArgs("cloud_confidence_two", position, "Cloud confidence bit two"));
+                position <<= 1; // 10
+                add(new FlagCodingArgs("cloud_shadow_confidence_one", position, "Cloud shadow confidence bit one"));
+                position <<= 1; // 11
+                add(new FlagCodingArgs("cloud_shadow_confidence_two", position, "Cloud shadow confidence bit two"));
+                position <<= 1; // 12
+                add(new FlagCodingArgs("snow_ice_confidence_one", position, "Snow/ice confidence bit one"));
+                position <<= 1; // 13
+                add(new FlagCodingArgs("snow_ice_confidence_two", position, "Snow/ice confidence bit two"));
+                position <<= 1; // 14
+                add(new FlagCodingArgs("cirrus_confidence_one", position, "Cirrus confidence bit one"));
+                position <<= 1; // 15
+                add(new FlagCodingArgs("cirrus_confidence_two", position, "Cirrus confidence bit two"));
+            }});
         put("FILE_NAME_QUALITY_L1_RADIOMETRIC_SATURATION",
-                new ArrayList<FlagCodingArgs>() {{
-                    int position = 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b1", position, "Band 1 data saturation"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b2", position, "Band 2 data saturation"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b3", position, "Band 3 data saturation"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b4", position, "Band 4 data saturation"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b5", position, "Band 5 data saturation"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b6", position, "Band 6 data saturation"));
-                    position <<= 1;
-                    add(new FlagCodingArgs("radiometric_saturation_b7", position, "Band 7 data saturation"));
-                    position <<= 2; // position 7 not used
-                    add(new FlagCodingArgs("radiometric_saturation_b9", position, "Band 9 data saturation"));
-                    position <<= 3; // positions 9 and 10 not used
-                    add(new FlagCodingArgs("terrain_occlusion", position, "Terrain occlusion"));
-                }});
+            new ArrayList<FlagCodingArgs>() {{
+                int position = 1;
+                add(new FlagCodingArgs("radiometric_saturation_b1", position, "Band 1 data saturation"));
+                position <<= 1;
+                add(new FlagCodingArgs("radiometric_saturation_b2", position, "Band 2 data saturation"));
+                position <<= 1;
+                add(new FlagCodingArgs("radiometric_saturation_b3", position, "Band 3 data saturation"));
+                position <<= 1;
+                add(new FlagCodingArgs("radiometric_saturation_b4", position, "Band 4 data saturation"));
+                position <<= 1;
+                add(new FlagCodingArgs("radiometric_saturation_b5", position, "Band 5 data saturation"));
+                position <<= 1;
+                add(new FlagCodingArgs("radiometric_saturation_b6", position, "Band 6 data saturation"));
+                position <<= 1;
+                add(new FlagCodingArgs("radiometric_saturation_b7", position, "Band 7 data saturation"));
+                position <<= 2; // position 7 not used
+                add(new FlagCodingArgs("radiometric_saturation_b9", position, "Band 9 data saturation"));
+                position <<= 3; // positions 9 and 10 not used
+                add(new FlagCodingArgs("terrain_occlusion", position, "Terrain occlusion"));
+            }});
     }};
 
     @Override
@@ -95,48 +97,79 @@ public class Collection2OLILandsatQA extends AbstractLandsatQA {
         final int height = size.height;
 
         masks.add(Mask.BandMathsType.create("designated_fill",
-                "Designated Fill",
-                width, height,
-                "flags.designated_fill",
-                colorIterator.next(),
-                0.5));
+                                            "Designated Fill",
+                                            width, height,
+                                            "flags.designated_fill",
+                                            colorIterator.next(),
+                                            0.5));
         masks.add(Mask.BandMathsType.create("terrain_occlusion",
-                "Terrain Occlusion",
-                width, height,
-                "satflags.terrain_occlusion",
-                colorIterator.next(),
-                0.5));
+                                            "Terrain Occlusion",
+                                            width, height,
+                                            "satflags.terrain_occlusion",
+                                            colorIterator.next(),
+                                            0.5));
+        masks.add(Mask.BandMathsType.create("cirrus",
+                                            "Cirrus",
+                                            width, height,
+                                            "flags.cirrus",
+                                            colorIterator.next(),
+                                            0.5));
         masks.add(Mask.BandMathsType.create("cloud",
-                "Cloud",
-                width, height,
-                "flags.cloud",
-                colorIterator.next(),
-                0.5));
+                                            "Cloud",
+                                            width, height,
+                                            "flags.cloud",
+                                            colorIterator.next(),
+                                            0.5));
+        masks.add(Mask.BandMathsType.create("cloud_shadow",
+                                            "Cloud Shadow",
+                                            width, height,
+                                            "flags.cloud_shadow",
+                                            colorIterator.next(),
+                                            0.5));
+        masks.add(Mask.BandMathsType.create("snow",
+                                            "Snow/Ice Cover",
+                                            width, height,
+                                            "flags.snow",
+                                            colorIterator.next(),
+                                            0.5));
+        masks.add(Mask.BandMathsType.create("clear",
+                                            "Clear",
+                                            width, height,
+                                            "flags.clear",
+                                            colorIterator.next(),
+                                            0.5));
+        masks.add(Mask.BandMathsType.create("water",
+                                            "Water",
+                                            width, height,
+                                            "flags.water",
+                                            colorIterator.next(),
+                                            0.5));
+
         //masks.addAll(createDefaultRadiometricSaturationMasks("radiometric_saturation", "Radiometric saturation", width, height));
         masks.add(Mask.BandMathsType.create("Saturation for band 1", "Saturation for band 1",
-                width, height, "satflags.radiometric_saturation_b1",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b1",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 2", "Saturation for band 2",
-                width, height, "satflags.radiometric_saturation_b2",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b2",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 3", "Saturation for band 3",
-                width, height, "satflags.radiometric_saturation_b3",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b3",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 4", "Saturation for band 4",
-                width, height, "satflags.radiometric_saturation_b4",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b4",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 5", "Saturation for band 5",
-                width, height, "satflags.radiometric_saturation_b5",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b5",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 6", "Saturation for band 6",
-                width, height, "satflags.radiometric_saturation_b6",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b6",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 7", "Saturation for band 7",
-                width, height, "satflags.radiometric_saturation_b7",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b7",
+                                            colorIterator.next(), 0.5));
         masks.add(Mask.BandMathsType.create("Saturation for band 9", "Saturation for band 9",
-                width, height, "satflags.radiometric_saturation_b9",
-                colorIterator.next(), 0.5));
+                                            width, height, "satflags.radiometric_saturation_b9",
+                                            colorIterator.next(), 0.5));
         masks.addAll(createDefaultConfidenceMasks("snow_ice_confidence", "Snow/ice confidence", width, height));
         masks.addAll(createDefaultConfidenceMasks("cloud_confidence", "Cloud confidence", width, height));
         masks.addAll(createDefaultConfidenceMasks("cloud_shadow_confidence", "Cloud shadow confidence", width, height));
